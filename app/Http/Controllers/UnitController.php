d459a6044b610965891687d7b91482440a7c7df7
mhafet17 <do.crazy192@gmail.com>
2020-08-07 17:05:23 +0700

    Add Unit Controllers

---
+++ app/Http/Controllers/UnitController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Unit;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class UnitController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        return view('admin.unit.index', [
+            'unit' => Unit::latest()->get()
+        ]);
+    }
+
+
+    public function create()
+    {
+        return view('admin.unit.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_unit'             => 'required|min:3|max:100',
+            'deskripsi'             => 'required|min:10',
+            'alamat'                => 'required|min:3|max:200',
+            'email'                 => 'required|email|unique:unit',
+            'gambar_unit'           => 'required|image|mimes:jpg,jpeg,png,bmp',
+            'whatsapp'              => 'required',
+            'telegram'              => 'required',
+            'instagram'             => 'required',
+            'username'              => 'required|max:100|unique:unit',
+            'password'              => 'required|min:3',
+            'konfirmasi_password'   => 'required|same:password|min:3'
+        ]);
+
+        $data = $request->all();
+        $nama_slug = $data['nama_unit'];
+        $data['slug'] = Str::slug($nama_slug, '-');
+        $data['password'] = Hash::make($data['password']);
+        $data['gambar_unit'] = $request->file('gambar_unit')->store('unit', 'public');
+
+        Unit::create($data);
+        return redirect()->route('unit.index')
+            ->with(['status' => 'Data Unit Berhasil Ditambahkan']);
+    }
+
+
+    public function show(Unit $unit)
+    {
+        return view('admin.unit.show', compact('unit'));
+    }
+
+
+    public function edit(Unit $unit)
+    {
+        return view('admin.unit.edit', compact('unit'));
+    }
+
+
+    public function update(Request $request, Unit $unit)
+    {
+        $request->validate([
+            'nama_unit'             => 'required|min:3|max:100',
+            'deskripsi'             => 'required|min:10',
+            'alamat'                => 'required|min:3|max:200',
+            'email'                 => 'required|email|unique:unit,email,' . $unit->id,
+            'gambar_unit'           => 'sometimes|nullable|image|mimes:jpg,jpeg,png,bmp',
+            'username'              => 'required|min:3|max:100|unique:unit,username,' . $unit->id,
+            'whatsapp'              => 'required',
+            'telegram'              => 'required',
+            'instagram'             => 'required',
+            'password'              => 'sometimes|nullable|min:3',
+            'konfirmasi_password'   => 'sometimes|same:password|nullable|min:3',
+            'status'                => 'required',
+        ]);
+
+        $data = $request->all();
+
+        if ($request->input('password')) {
+            $data['password'] = Hash::make($data['password']);
+        } else {
+            $data = Arr::except($data, ['password']);
+        }
+
+        if ($request->hasFile('gambar_unit')) {
+            if ($unit->gambar_unit && file_exists(storage_path('app/public/' . $unit->gambar_unit))) {
+                Storage::delete('public/' . $unit->gambar_unit);
+                $data['gambar_unit'] =  $request->file('gambar_unit')->store('unit', 'public');
+            }
+        }
+
+        $unit->update($data);
+        return redirect()->route('unit.index')->with([
+            'status' => 'Data unit Berhasil Di Update'
+        ]);
+    }
+
+
+    public function destroy(Unit $unit)
+    {
+        Storage::delete('public/' . $unit->gambar_unit);
+        $unit->forceDelete();
+        return redirect()->route('unit.index')
+            ->with(['status' => 'Data unit Berhasil Dihapus']);
+    }
+}
