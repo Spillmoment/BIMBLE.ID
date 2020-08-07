2747dafd79c98fb554cef77fdafe0a06253de12f
mhafet17 <do.crazy192@gmail.com>
2020-08-07 17:02:25 +0700

    Add Controllers AuthUnit

---
+++ app/Http/Controllers/AuthUnit/LoginController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\AuthUnit;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:unit')->except('logoutUnit');
+    }
+
+    public function showLoginForm()
+    {
+        return view('authUnit.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:3',
+        ]);
+
+        $credential = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (Auth::guard('unit')->attempt($credential, $request->member)) {
+            return redirect()->intended(route('unit.home'));
+        } else {
+            session()->flash('loginError', 'Periksa kembali login anda');
+        }
+
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+    }
+
+    public function logoutUnit()
+    {
+        Auth::guard('unit')->logout();
+        return redirect('/unit/login');
+    }
+}
---
+++ app/Http/Controllers/AuthUnit/ResetPasswordController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\AuthUnit;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Auth;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/unit';
+
+    public function __construct()
+    {
+        $this->middleware('guest:unit');
+    }
+
+    public function guard()
+    {
+        return Auth::guard('unit');
+    }
+
+    public function broker()
+    {
+        return Password::broker('units');
+    }
+
+    /**
+     * Get the password reset validation rules.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6',
+        ];
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('AuthUnit.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+}
