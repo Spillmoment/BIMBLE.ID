356e8f50080a5baf45fbe6b4181731ad440b1dd1
mhafet17 <do.crazy192@gmail.com>
2020-08-07 17:06:39 +0700

    Modifikasi model kursus

--- app/Kursus.php
+++ app/Kursus.php
@@ -8,45 +8,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Kursus extends Model
 {
     protected $table = 'kursus';
-
     protected $primaryKey = 'id';
+
     protected $fillable = [
-        'id_kategori', 'id_tutor', 'nama_kursus', 'slug', 'gambar_kursus', 'biaya_kursus', 'diskon_kursus', 'lama_kursus',
-        'latitude', 'longitude', 'keterangan'
+        'nama_kursus', 'slug', 'gambar_kursus', 'keterangan'
     ];
 
-
     use SoftDeletes;
-
-
-    public function kategori()
-    {
-        return $this->hasMany(Kategori::class, 'id', 'id_kategori');
-    }
-
-    public function tutor()
-    {
-        return $this->hasMany(Tutor::class, 'id', 'id_tutor');
-    }
-
-    public function galleries()
-    {
-        return $this->hasMany(Gallery::class, 'kursus_id', 'id');
-    }
-
-
-    public function order_detail()
-    {
-        return $this->belongsTo(OrderDetail::class, 'id', 'id_kursus');
-    }
-
-    public function komentar()
-    {
-        return $this->belongsTo(Komentar::class, 'id', 'id_kursus');
-    }
-
-    public function nilai()
-    {
-        return $this->belongsTo(Nilai::class, 'id', 'id_kursus');
-    }
 }
