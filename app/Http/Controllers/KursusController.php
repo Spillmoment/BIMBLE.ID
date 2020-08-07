f2058ad703cb77fbbf43197eef29de90caa45c3c
mhafet17 <do.crazy192@gmail.com>
2020-08-07 17:05:04 +0700

    Modifikasi Controller Kursus Baru

--- app/Http/Controllers/KursusController.php
+++ app/Http/Controllers/KursusController.php
@@ -5,10 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Kursus;
 use App\Http\Requests\KursusRequest;
-use App\Kategori;
-use App\Gallery;
-use App\Tutor;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -23,31 +19,7 @@ class KursusController extends Controller
 
     public function index()
     {
-
-        // $kategori = Kategori::orderBy('created_at', 'DESC')->paginate(10);
-
-        // $keyword = $request->get('keyword');
-        // $filter_kategori = $request->get('nama_kategori');
-
-        // if ($keyword) {
-        //     $kursus = Kursus::with(['kategori', 'tutor'])
-        //         ->where('nama_kursus', 'LIKE', "%$keyword%")
-        //         ->paginate(10);
-        // } else {
-        //     $kursus = Kursus::orderBy('created_at', 'DESC')->paginate(10);
-        // }
-
-        // if ($filter_kategori) {
-        //     $kursus = Kursus::with(['kategori', 'tutor'])
-        //         ->where('id_kategori', 'LIKE', "%$filter_kategori%")
-        //         ->orderBy('created_at', 'DESC')
-        //         ->paginate(10);
-        // }
-
-        $kursus = Kursus::with('kategori')
-            ->orderBy('created_at', 'DESC')
-            ->paginate(10);
-        return view('admin.kursus.index', compact('kursus'));
+        return view('admin.kursus.index', ['kursus' => Kursus::latest()->get()]);
     }
 
     /**
@@ -55,12 +27,9 @@ class KursusController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Kategori $kategori, Tutor $tutor)
+    public function create()
     {
-        return view('admin.kursus.create', [
-            'kategori' => $kategori->all(),
-            'tutor'    => $tutor->all()
-        ]);
+        return view('admin.kursus.create');
     }
 
     /**
@@ -100,13 +69,9 @@ class KursusController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Kursus $kursus, Kategori $kategori, Tutor $tutor)
+    public function edit(Kursus $kursus)
     {
-        return view('admin.kursus.edit', [
-            'kursus'   => $kursus,
-            'tutor'    => $tutor->all(),
-            'kategori' => $kategori->all()
-        ]);
+        return view('admin.kursus.edit', compact('kursus'));
     }
 
     /**
@@ -126,7 +91,7 @@ class KursusController extends Controller
         if ($request->hasFile('gambar_kursus')) {
             if ($kursus->gambar_kursus && file_exists(storage_path('app/public/' . $kursus->gambar_kursus))) {
                 Storage::delete('public/' . $kursus->gambar_kursus);
-                $data['gambar_kursus'] = $request->file('gambar_kursus')->store('kursus', 'public');
+                $data['gambar_kursus'] =  $request->file('gambar_kursus')->store('kursus', 'public');
             }
         }
 
@@ -142,66 +107,9 @@ class KursusController extends Controller
      */
     public function destroy(Kursus $kursus)
     {
-        File::delete('uploads/kursus/' . $kursus->gambar_kursus);
-        $kursus->delete();
+        Storage::delete('public/' . $kursus->gambar_kursus);
+        $kursus->forceDelete();
         return redirect()->route('kursus.index')
             ->with(['status' => 'Data Kursus Berhasil Dihapus']);
-    }
-
-    public function gallery($id)
-    {
-        $kursus = Kursus::with('galleries')
-            ->findorFail($id);
-
-        $gallery = Gallery::with('kursus')
-            ->where('kursus_id', $id)
-            ->orderBy('created_at', 'DESC')
-            ->paginate(9);
-
-        return view('admin.kursus.gallery')->with([
-            'kursus' => $kursus,
-            'items' => $gallery
-        ]);
-    }
-
-    public function trash(Request $request)
-    {
-        $filter_trash = $request->get('trash');
-
-        if ($filter_trash) {
-            $kursus = Kursus::onlyTrashed()->where('nama_kursus', 'LIKE', "%$filter_trash%")
-                ->paginate(10);
-        } else {
-            $kursus = Kursus::onlyTrashed()->paginate(10);
-        }
-
-        return view('admin.kursus.trash', compact('kursus'));
-    }
-
-    public function restore($id)
-    {
-        $kursus = Kursus::withTrashed()->findOrFail($id);
-
-        if ($kursus->trashed()) {
-            $kursus->restore();
-        } else {
-            return redirect()->route('kursus.index')
-                ->with(['status' => 'kursus tidak ada di Trash']);
-        }
-        return redirect()->route('kursus.index')
-            ->with(['status' => 'kursus Sukses Dikembalikan']);
-    }
-
-    public function deletePermanent($id)
-    {
-        $kursus = Kursus::withTrashed()->findOrFail($id);
-        if (!$kursus->trashed()) {
-            return redirect()->route('kursus.trash')
-                ->with('status', 'Tidak bisa menghapus permanent data kursus yang aktif');
-        } else {
-            $kursus->forceDelete();
-            return redirect()->route('kursus.trash')
-                ->with('status', 'Data kursus berhasil dihapus permanent');
-        }
     }
 }
