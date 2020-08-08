c79380a4473800a146010ce2bbff37ec6b7758b9
mhafet17 <do.crazy192@gmail.com>
2020-08-08 16:15:40 +0700

    replace style

--- resources/views/web/layouts/style.blade.php
+++ resources/views/web/layouts/style.blade.php
@@ -1,18 +1,18 @@
 <link rel="stylesheet" href="{{ asset('assets/frontend/vendor/nouislider/nouislider.css') }}">
-  <!-- Google fonts - Playfair Display-->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
-  <!-- Google fonts - Poppins-->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
-  <!-- Magnigic Popup-->
-  <link rel="stylesheet" href="{{asset('assets/frontend/vendor/magnific-popup/magnific-popup.css')}}">
-  <!-- theme stylesheet-->
-  <link rel="stylesheet" href="{{ asset('assets/frontend/vendor/bootstrap/style.default.css')}}" id="theme-stylesheet">
-  <!-- Custom stylesheet - for your changes-->
-  <link rel="stylesheet" href="{{asset('assets/frontend/css/custom.css')}}">
-  <!-- Favicon-->
-  <link rel="shortcut icon" href="{{ asset('assets/frontend/img/favicon.png') }}">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-  <!-- Owl carousel -->
-  <link rel="stylesheet" href="{{ asset('assets/frontend/vendor/owl-carousel/dist/assets/owl.carousel.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/frontend/vendor/owl-carousel/dist/assets/owl.theme.default.css') }}">
+<!-- Google fonts - Playfair Display-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
+<!-- Google fonts - Poppins-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
+<!-- Magnigic Popup-->
+<link rel="stylesheet" href="{{asset('assets/frontend/vendor/magnific-popup/magnific-popup.css')}}">
+<!-- theme stylesheet-->
+<link rel="stylesheet" href="{{ asset('assets/frontend/vendor/bootstrap/style.default.css')}}" id="theme-stylesheet">
+<!-- Custom stylesheet - for your changes-->
+<link rel="stylesheet" href="{{asset('assets/frontend/css/custom.css')}}">
+<!-- Favicon-->
+<link rel="shortcut icon" href="{{ asset('assets/frontend/img/favicon.png') }}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+<!-- Owl carousel -->
+<link rel="stylesheet" href="{{ asset('assets/frontend/vendor/owl-carousel/dist/assets/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/frontend/vendor/owl-carousel/dist/assets/owl.theme.default.css') }}">
