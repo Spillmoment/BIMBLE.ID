5d6e450eaeccb7e213d915ac014ca8bcc409ed7d
gunawan <deddygunawan98@gmail.com>
2020-08-09 11:02:44 +0700

    add cdn ckeditor

--- resources/views/admin/includes/style.blade.php
+++ resources/views/admin/includes/style.blade.php
@@ -17,6 +17,7 @@
 <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
 
+<script src="https://cdn.ckeditor.com/ckeditor5/21.0.0/classic/ckeditor.js"></script>
 
 <style>
     #weatherWidget .currentDesc {
