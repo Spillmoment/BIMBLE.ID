8c47dd601318b30f977df1444490bec3b8933dfe
mhafet17 <do.crazy192@gmail.com>
2020-08-10 22:51:47 +0700

    add function index unit

--- app/Http/Controllers/Web/UnitController.php
+++ app/Http/Controllers/Web/UnitController.php
@@ -8,6 +8,16 @@ use App\Unit;
 
 class UnitController extends Controller
 {
+
+    public function index()
+    {
+        return view('web.web_unit');
+    }
+
+    public function post()
+    {
+    }
+
     public function show($slug)
     {
         $unit = Unit::where('slug', $slug)->firstOrFail();
