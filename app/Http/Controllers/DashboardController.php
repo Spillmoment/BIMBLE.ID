38b42d0b77199a3d532c19a467aec46e871daded
mhafet17 <do.crazy192@gmail.com>
2020-08-18 13:39:20 +0700

    tambah statistik dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Kursus;
 use App\Unit;
+use App\Komentar;
 
 class DashboardController extends Controller
 {
@@ -12,13 +13,17 @@ class DashboardController extends Controller
     public function index()
     {
         $kursus = Kursus::all()->count();
-        $unit = Unit::all()->count();
+        $unit = Unit::where('status', '1')->count();
+        $pendaftar = Unit::where('status', '0')->count();
+        $komentar = Komentar::all()->count();
 
         return view(
             'admin.dashboard.index',
             [
                 'kursus' => $kursus,
                 'unit' => $unit,
+                'pendaftar' => $pendaftar,
+                'komentar' => $komentar,
             ]
         );
     }
