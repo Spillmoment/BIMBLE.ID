dd81d2c42f82e1e504dc174568ffab07a397d537
mhafet17 <do.crazy192@gmail.com>
2020-08-18 13:48:04 +0700

    web unit

--- resources/views/web/web_unit.blade.php
+++ resources/views/web/web_unit.blade.php
@@ -3,29 +3,67 @@
 @section('title','Bimble.id | Daftar Unit')
 @section('content')
 
-<section class="pt-5 pb-6 bg-gray-100">
+<section class="pt-4 pb-6 bg-gray-100">
+    <div class="container position-relative">
+      <!-- Breadcrumbs -->
+      <ol class="breadcrumb pl-0  justify-content-center">
+        <li class="breadcrumb-item"><a href="{{ route('front.index') }}">Home</a></li>
+        <li class="breadcrumb-item active">Daftar Unit</li>
+      
+    </div>
     <div class="container">
         <h2 class="h4 mb-3">Form Pendaftran Unit</h2>
         <div class="row">
+  
             <div class="col-md-7 mb-5 mb-md-0">
-                <form id="contact-form" method="post" action="http://demo.bootstrapious.com/directory/1-4-1/contact.php"
-                    class="form">
+                @if (session('message'))
+                   <div class="alert alert-success alert-dismissible fade show" role="alert">
+                       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                           <span aria-hidden="true">&times;</span>
+                           <span class="sr-only">Close</span>
+                       </button>
+                       <strong>{{ session('message') }}</strong>
+                   </div>
+                    @endif
+                <form id="contact-form" method="post" action="{{ route('unit.add') }}"
+                    class="form" enctype="multipart/form-data">
+                    @csrf
                     <div class="controls">
                         <div class="form-group">
                             <label for="surname" class="form-label">Nama Unit</label>
-                            <input type="text" name="surname" id="surname" placeholder="Masukkan Nama Unit"
-                                required="required" class="form-control">
-                        </div>
+                            <input type="text" name="nama_unit" id="surname" placeholder="Masukkan Nama Unit"
+                                required="required" class="form-control {{ $errors->first('nama_unit') ? 'is-invalid' : '' }}" value="{{ old('nama_unit') }}">
+                                <div class="invalid-feedback">
+                                    {{$errors->first('nama_unit')}}
+                                </div>
+                            </div>
                         <div class="form-group">
                             <label for="email" class="form-label">Email</label>
                             <input type="email" name="email" id="email" placeholder="Masukkan Email" required="required"
-                                class="form-control">
+                                class="form-control {{ $errors->first('email') ? 'is-invalid' : '' }}" value="{{ old('email') }}">
+                                <div class="invalid-feedback">
+                                    {{$errors->first('email')}}
+                                </div>
                         </div>
                         <div class="form-group">
                             <label for="message" class="form-label">Alamat</label>
-                            <textarea rows="4" name="message" id="message" placeholder="Masukkan Alamat"
-                                required="required" class="form-control"></textarea>
+                            <textarea rows="4" name="alamat" id="message" placeholder="Masukkan Alamat"
+                                required="required" class="form-control {{ $errors->first('alamat') ? 'is-invalid' : '' }}">{{ old('alamat') }}
+                            </textarea>
+                            <div class="invalid-feedback">
+                                {{$errors->first('alamat')}}
+                            </div>
                         </div>
+                        <div class="form-group">
+                          <label for="bukti">File Alumni</label>
+                          <input type="file" class="form-control-file" name="bukti_alumni" id="bukti" placeholder="" aria-describedby="fileHelpId">
+                          <small id="fileHelpId" class="form-text text-muted {{ $errors->first('bukti_alumni') ? 
+                          'is-invalid' : '' }}">Upload File Bukti ALumni</small>
+                        <div class="invalid-feedback">
+                            {{$errors->first('bukti_alumni')}}
+                        </div>
+                        </div>
+                        <button type="reset" class="btn btn-outline-danger">Reset</button>
                         <button type="submit" class="btn btn-outline-primary">Kirim</button>
                     </div>
                 </form>
@@ -33,7 +71,7 @@
             <div class="col-md-5">
                 <div class="pl-lg-4">
                     <p class="text-muted">
-                        Silahkan cantumkan detail unit anda
+                        Silahkan isi form untuk pendaftaran unit beserta bukti form alumni unuja
                     </p>
                     <p class="text-muted">
                         Untuk selengkapnya hubungi kami
