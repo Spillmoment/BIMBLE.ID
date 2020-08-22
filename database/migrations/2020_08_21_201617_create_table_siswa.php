7179dc493e7e5a439ee6fb3abdd4708313151df7
gunawan <deddygunawan98@gmail.com>
2020-08-22 07:13:04 +0700

    create siswa migration

---
+++ database/migrations/2020_08_21_201617_create_table_siswa.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableSiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('kursus_id');
+            $table->foreign('kursus_id')->references('id')->on('kursus')->onDelete('cascade');
+
+            $table->foreignId('unit_id');
+            $table->foreign('unit_id')->references('id')->on('unit')->onDelete('cascade');
+
+            $table->string('nama_siswa', 100);
+            $table->enum('jenis_kelamin', ['L', 'P'])->nullable();
+            $table->text('alamat')->nullable();
+            $table->double('nilai', 8, 2)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
