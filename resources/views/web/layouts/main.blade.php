5a5aaa8ce38e1b213b598b90d188092b58c024a5
mhafet17 <do.crazy192@gmail.com>
2020-08-27 10:29:53 +0700

    modikasi layout web

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{asset('assets/frontend/img/favicon.png') }}" type="image/x-icon">
     <title>@yield('title')</title>
-    @stack('style')    
+    @stack('style')
 </head>
 
 <body style="padding-top: 72px;">
 
     @include('web.layouts.style')
 
+    @stack('style')
+
     @include('web.layouts.header')
 
     @yield('content')
 
     @include('web.layouts.footer')
-    
+
     @include('web.layouts.script')
 
     @stack('scripts')
 </body>
+
 </html>
