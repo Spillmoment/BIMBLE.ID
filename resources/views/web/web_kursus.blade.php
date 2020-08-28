63cb584587b853d280b193aaa7a6c78c0ce3bc48
gunawan <deddygunawan98@gmail.com>
2020-08-28 07:48:24 +0700

    hapus label type di list semua kursus

--- resources/views/web/web_kursus.blade.php
+++ resources/views/web/web_kursus.blade.php
@@ -56,10 +56,10 @@
                             <img src="{{ Storage::url('public/'. $item->kursus->gambar_kursus) }}"
                                 alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" />
                             <a href="{{ route('front.detail', $item->kursus->slug) }}" class="tile-link"></a>
-                            <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
+                            {{-- <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
                                 <div class="badge badge-transparent badge-pill px-3 py-2">{{ $item->type->nama_type }}
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                         <div class="card-body d-flex align-items-center">
                             <div class="w-100">
