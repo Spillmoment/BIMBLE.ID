9cf868806bb0f393a641d0e3193ab19eacaedc8e
mhafet17 <do.crazy192@gmail.com>
2020-09-03 08:12:45 +0700

    tambah validasi form pendaftaran unit

--- resources/views/web/web_unit.blade.php
+++ resources/views/web/web_unit.blade.php
@@ -4,17 +4,17 @@
 @section('content')
 
 <section class="pt-4 pb-6 bg-gray-100">
+    
     <div class="container position-relative">
       <!-- Breadcrumbs -->
       <ol class="breadcrumb pl-0  justify-content-center">
         <li class="breadcrumb-item"><a href="{{ route('front.index') }}">Home</a></li>
         <li class="breadcrumb-item active">Daftar Unit</li>
-      
     </div>
+
     <div class="container">
-        <h2 class="h4 mb-3">Form Pendaftran Unit</h2>
+        <h2 class="h4 mb-3 mt-5">Form Pendaftran Unit</h2>
         <div class="row">
-  
             <div class="col-md-7 mb-5 mb-md-0">
                 @if (session('message'))
                    <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -29,20 +29,7 @@
                     class="form" enctype="multipart/form-data">
                     @csrf
                     <div class="controls">
-
-                        <div class="form-group">
-                            <label for="bukti" class="form-label font-weight-bold">* Kursus Yang Harus Dipersiapkan</label>
-                         <div class="invalid-feeback">
-
-                             @foreach ($kursus as $item)
-                             <span class="badge badge-dark badge-pill  mr-1">
-                                 {{ $item->nama_kursus }}
-                                </span>
-                                @endforeach
-                            </div>
-                                
-                        </div>
-                        
+                                                    
                         <div class="form-group">
                             <label for="surname" class="form-label">Nama Unit</label>
                             <input type="text" name="nama_unit" id="surname" placeholder="Masukkan Nama Unit"
@@ -69,6 +56,7 @@
                                     {{$errors->first('email')}}
                                 </div>
                         </div>
+
                         <div class="form-group">
                             <label for="message" class="form-label">Alamat</label>
                             <textarea rows="4" name="alamat" id="message" placeholder="Masukkan Alamat"
@@ -89,14 +77,22 @@
                         </div>
                         </div>
 
-                        
                         <button type="reset" class="btn btn-outline-danger">Reset</button>
                         <button type="submit" class="btn btn-outline-primary">Kirim</button>
                     </div>
                 </form>
             </div>
+          
             <div class="col-md-5">
                 <div class="pl-lg-4">
+                 
+                    <div class="mb-3">
+                        <h2 class="h4 mb-3">Kursus Yang Dipersiapkan</h2>
+                            @foreach ($kursus as $item)
+                        <span class="badge badge-pill badge-default p-3 text-muted font-weight-bold">{{ $item->nama_kursus }}</span>
+                            @endforeach
+                    </div>
+                
                     <p class="text-muted">
                         Silahkan isi form untuk pendaftaran unit beserta bukti form alumni unuja
                     </p>
@@ -128,7 +124,7 @@
 @push('scripts')
     <script>
         $(document).ready(function() {
-            $('#registerForm').bootstrapValidator({
+            $('#contact-form').bootstrapValidator({
                 message: 'This value is not valid',
                 feedbackIcons: {
                     valid: 'glyphicon glyphicon-ok',
@@ -136,36 +132,24 @@
                     validating: 'glyphicon glyphicon-refresh'
                 },
                 fields: {
-                    nama_lengkap: {
+                    nama_unit: {
                         validators: {
                             notEmpty: {
+                                message: 'Silahkan isi nama unit'
                             },
                             stringLength: {
-                                min: 6,
-                                max: 30,
+                                min: 3,
+                                max: 100,
                             },
                             regexp: {
                                 regexp: /^[a-zA-Z ]+$/,
                             }
                         }
                     },
-                    username: {
+                    no_telp: {
                         validators: {
                             notEmpty: {
-                            },
-                            stringLength: {
-                                min: 5,
-                                max: 30,
-                            },
-                            regexp: {
-                                regexp: /^[a-zA-Z0-9_]+$/,
-                                message: ' username hanya dapat terdiri dari abjad, angka, dan garis bawah'
-                            }
-                        }
-                    },
-                    nomor_hp: {
-                        validators: {
-                            notEmpty: {
+                                message: 'Silahkan isi no telepon'
                             },
                             stringLength: {
                                 min: 11,
@@ -173,17 +157,17 @@
                             },
                             regexp: {
                                 regexp: /^[0-9+]+$/,
-                                message: " Silahkan isi dengan hanya angka"
+                                // message: " Silahkan isi dengan angka"
                             }
                         }
                     },
                     email: {
                         validators: {
                             notEmpty: {
-                                message: 'The email is required and cannot be empty'
+                                message: 'Silahkan isi email'
                             },
                             emailAddress: {
-                                message: 'The input is not a valid email address'
+                                message: 'Masukkan alamat email yang valid'
                             }
                         }
                     },
