5c10a590f5f006b5b19de7a2748353613fc21892
gunawan <deddygunawan98@gmail.com>
2020-09-05 04:36:14 +0700

    home blade

--- resources/views/web/web_home.blade.php
+++ resources/views/web/web_home.blade.php
@@ -186,7 +186,7 @@
                                 <div class="card-img-top overflow-hidden gradient-overlay">
                                     <img src="{{ Storage::url('public/'. $item->kursus->gambar_kursus) }}"
                                         alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" /><a
-                                        href="{{ route('front.detail', $item->kursus->slug) }}" class="tile-link"></a>
+                                        href="{{ route('front.detail.kelompok', $item->kursus->slug) }}" class="tile-link"></a>
                                     {{-- <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
                                         <div class="badge badge-transparent badge-pill px-3 py-2">
                                             {{ $item->type->nama_type }}</div>
@@ -195,7 +195,7 @@
                                 <div class="card-body d-flex align-items-center">
                                     <div class="w-100">
                                         <h6 class="card-title"><a
-                                                href="{{ route('front.detail', $item->kursus->slug) }}"
+                                                href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
                                                 class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a>
                                         </h6>
                                         {{-- <div class="d-flex card-subtitle mb-3">
