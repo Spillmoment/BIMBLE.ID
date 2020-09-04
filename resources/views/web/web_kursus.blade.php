83ac77f503a296dda107af61474c8f743554af2c
gunawan <deddygunawan98@gmail.com>
2020-09-05 04:36:02 +0700

    blade kursus

--- resources/views/web/web_kursus.blade.php
+++ resources/views/web/web_kursus.blade.php
@@ -19,8 +19,7 @@
 
                 <div class="mb-4">
                     <label for="form_category" class="form-label">Type Kursus</label>
-                    <select name="type" id="form_category" multiple data-style="btn-selectpicker"
-                        data-selected-text-format="count &gt; 1" title="" class="selectpicker form-control">
+                    <select name="type" id="form_category" data-style="btn-selectpicker" title="" class="selectpicker form-control">
                         @foreach ($typeKursus as $item)
                         <option value="{{ $item->id }}">{{ $item->nama_type }}</option>
                         @endforeach
@@ -55,18 +54,18 @@
                         <div class="card-img-top overflow-hidden gradient-overlay">
                             <img src="{{ Storage::url('public/'. $item->kursus->gambar_kursus) }}"
                                 alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" />
-                            <a href="{{ route('front.detail', $item->kursus->slug) }}" class="tile-link"></a>
-                            {{-- @if (Request::get('keyword'))
+                            <a href="{{ Request::get('type') == 2 ? route('front.detail.private', $item->kursus->slug) : route('front.detail.kelompok', $item->kursus->slug) }}" class="tile-link"></a>
+                            {{-- @if (Request::get('keyword')) --}}
                                 <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
                                     <div class="badge badge-transparent badge-pill px-3 py-2">
                                         {{ $item->type->nama_type }}
                                     </div>
                                 </div>                                
-                            @endif --}}
+                            {{-- @endif --}}
                         </div>
                         <div class="card-body d-flex align-items-center">
                             <div class="w-100">
-                                <h6 class="card-title"><a href="{{ route('front.detail', $item->kursus->slug) }}"
+                                <h6 class="card-title"><a href="{{ Request::get('type') == 2 ? route('front.detail.private', $item->kursus->slug) : route('front.detail.kelompok', $item->kursus->slug) }}"
                                         class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a></h6>
                                 <div class="d-flex card-subtitle mb-3">
                                     <p class="flex-grow-1 mb-0 text-muted text-sm"></p>
@@ -77,9 +76,16 @@
                     </div>
                 </div>
                 @empty
-                <div class="mt-5 col alert alert-warning text-center" role="alert">
-                    <strong>Pencarian tidak ditemukan </strong>
-                    <a href="{{ route('front.kursus') }}" class="btn btn-warning">Kembali</a>
+                <div class="col-md-6 offset-md-3 text-center">
+                    <img width="300px" src="{{ asset('assets/frontend/img/schedule.gif') }}" alt="" srcset="">
+                    <h3 class="text-warning">Whoops!</h1>
+                    <p> Kursus tersebut segera hadir.
+                        <br>
+                        Silahkan cari dilain kesempatan.
+                    </p>
+                    <a href="{{ route('front.kursus') }}" class="btn btn-outline-primary btn-sm mt-3 px-5">
+                        Muat ulang
+                    </a>
                 </div>
                 @endforelse
 
