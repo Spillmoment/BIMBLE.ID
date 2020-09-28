b3ffd3e70511a90ef888235099e5bae0f0f3e389
mhafet17 <do.crazy192@gmail.com>
2020-09-28 23:02:21 +0700

    refactor link sidebar tutor

--- resources/views/admin/includes/sidebar-tutor.blade.php
+++ resources/views/admin/includes/sidebar-tutor.blade.php
@@ -6,45 +6,34 @@
 
                 <li class="{{ 
                     (Request::route()->getName() == 'unit.home') ? 'active' : '' }}
-                   {{  }} (Request::route()->getName() == 'unit.profile') ? 'active' : ''
+                   {{   (Request::route()->getName() == 'unit.profile') ? 'active' : '' }}
                     ">
                     <a href="{{ route('unit.home') }}"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
                 </li>
 
-
                 <li class="menu-title text-capitalize">Manage Kursus</li>
-                <li class="{{ 
-                    (Request::route()->getName() == 'unit.kursus.home') ? 'active' : ''
-                    }}">
+                <li class="{{ (request()->is('unit/kursus*')) ? 'active' : '' }}">
                     <a href="{{ route('unit.kursus.home') }}"> <i class="menu-icon fa fa-book"></i>Kursus</a>
                 </li>
 
                 <li class="menu-title text-capitalize">Manage Mentor</li>
-                <li class="{{ 
-                    (Request::route()->getName() == 'mentor.index') ? 'active' : ''
-                    }}">
+                <li class="{{ (request()->is('unit/mentor*')) ? 'active' : '' }} ">
                     <a href="{{ route('mentor.index') }}"> <i class="menu-icon fa fa-users"></i>Mentor</a>
                 </li>
 
                 <li class="menu-title text-capitalize">Manage Galeri</li>
-                <li class="{{ 
-                    (Request::route()->getName() == 'unit.galeri.home') ? 'active' : ''
-                    }}">
+                <li class="{{  (request()->is('unit/galeri*')) ? 'active' : '' }}">
                     <a href="{{ route('unit.galeri.home') }}"> <i class="menu-icon fa fa-camera"></i>Galeri</a>
                 </li>
 
                 <li class="menu-title text-capitalize">Manage Fasilitas</li>
-                <li class="{{ 
-                    (Request::route()->getName() == 'unit.fasilitas.home') ? 'active' : ''
-                    }}">
+                <li class="{{  (request()->is('unit/fasilitas*')) ? 'active' : ''  }}">
                     <a href="{{ route('unit.fasilitas.home') }}"> <i class="menu-icon fa fa-building"></i>Fasilitas</a>
                 </li>
                 
                 
                 <li class="menu-title text-capitalize">Manage Siswa</li>
-                <li class="{{ 
-                    (Request::route()->getName() == 'unit.siswa.home') ? 'active' : ''
-                    }}">
+                <li class="{{ (request()->is('unit/siswa*')) ? 'active' : '' }}">
                     <a href="{{ route('unit.siswa.home') }}"> <i class="menu-icon fa fa-pencil-square-o"></i>Nilai Siswa</a>
                 </li>
 
