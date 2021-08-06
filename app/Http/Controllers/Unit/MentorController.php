d2f19530a6c82efbba2a1f4c617aad2391d6d3bf
hafet17 <do.crazy192@gmail.com>
2021-08-06 17:42:27 +0700

    clean code mentor

--- app/Http/Controllers/Unit/MentorController.php
+++ app/Http/Controllers/Unit/MentorController.php
@@ -12,38 +12,24 @@ use Illuminate\Support\Str;
 
 class MentorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
     public function index()
     {
-        return view('unit.mentor.index', ['mentor' => Mentor::where('unit_id', Auth::id())->get()]);
+        return view('unit.mentor.index', [
+            'mentor' => Mentor::where('unit_id', Auth::id())->get()
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         return view('unit.mentor.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        $id_unit = Auth::id();
         Mentor::create([
-            'unit_id' => $id_unit,
+            'unit_id' => Auth::id(),
             'nama_mentor' => $request->nama_mentor,
             'kompetensi' => $request->kompetensi,
             'foto'  => $request->file('foto')->store('mentor', 'public'),
@@ -53,35 +39,19 @@ class MentorController extends Controller
             ->with(['status' => 'Data Mentor Berhasil Ditambahkan']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Mentor $mentor)
     {
         // return view('unit.mentor.show', compact('mentor'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Mentor $mentor)
     {
         return view('unit.mentor.edit', compact('mentor'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(MentorRequest $request, Mentor $mentor)
     {
         $data = $request->all();
@@ -99,12 +69,7 @@ class MentorController extends Controller
         return redirect()->route('mentor.index')->with(['status' => 'Data Mentor Berhasil Di Update']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Mentor $mentor)
     {
         Storage::delete('public/' . $mentor->foto);
