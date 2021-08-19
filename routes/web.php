bd974c22f9fe63677c52cc34a86fd16ba160e5a2
hafet17 <do.crazy192@gmail.com>
2021-08-19 11:42:32 +0700

    refactor route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Kursus;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 // Auth User
 // Auth Manager
