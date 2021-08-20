75da8ddc41cc69efff3d9ed861c41456aff90749
hafet17 <do.crazy192@gmail.com>
2021-08-20 20:35:34 +0700

    add label kelompok

--- resources/views/web/web_detail_kursus_kelompok.blade.php
+++ resources/views/web/web_detail_kursus_kelompok.blade.php
@@ -22,7 +22,7 @@
     <div class="container overlay-content">
         <div class="d-flex justify-content-between align-items-start flex-column flex-lg-row align-items-lg-end">
             <div class="text-white mb-4 mb-lg-0">
-
+                <div class="badge badge-pill badge-transparent px-3 py-2 mb-4 text-capitalize">Kursus Kelompok</div>
                 <h1 class="text-shadow verified">{{ $kursus->nama_kursus  }}</h1>
                 <p><i class="fa-map-marker-alt fas mr-2"></i> Paiton, Probolinggo</p>
 
