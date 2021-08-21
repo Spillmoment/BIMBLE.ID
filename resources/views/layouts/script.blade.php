435e7b4c2e39a4a3859b7854d5a0108bad93798a
hafet17 <do.crazy192@gmail.com>
2021-08-22 00:07:38 +0700

    delete layouts login

--- resources/views/layouts/script.blade.php
+++
@@ -1,3 +0,0 @@
-   <!-- jQuery-->
-   <script src="{{asset('assets/backend/bootstrap/js/jquery-3.4.1.min.js')}}"></script>
-   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
