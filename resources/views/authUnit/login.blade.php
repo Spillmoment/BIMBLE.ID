aee2e939e16882de4204b25c16a1abb3067d5c97
hafet17 <do.crazy192@gmail.com>
2021-08-22 00:07:29 +0700

    refactor login unit

--- resources/views/authUnit/login.blade.php
+++ resources/views/authUnit/login.blade.php
@@ -1,8 +1,18 @@
-@include('layouts.style')
+<!-- Google fonts - Playfair Display-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
+<!-- Google fonts - Poppins-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
+<!-- Magnigic Popup-->
+<link rel="stylesheet" href="{{asset('assets/frontend/vendor/magnific-popup/magnific-popup.css') }}">
+<!-- theme stylesheet-->
+<link rel="stylesheet" href="{{asset('assets/frontend/vendor/bootstrap/style.default.css') }}" id="theme-stylesheet">
+<!-- Custom stylesheet - for your changes-->
+<link rel="stylesheet" href="{{asset('assets/frontend/css/custom.css') }}">
+<!-- Favicon-->
+<link rel="shortcut icon" href="{{asset('assets/frontend/img/favicon.png') }}">
 
-<title> Unit | Login </title>
 
-
+<title> Bimble | Unit Login </title>
 <main class="login-container">
     <div class="container">
         <div class="row page-login d-flex justify-content-center">
@@ -20,8 +30,8 @@
                             <div class="alert alert-warning" role="alert">
                                 <h6>{{ session('loginError') }}</h6>
                             </div>
-                             @endif
- 
+                            @endif
+
                         </div>
                         <!--end auth-logo-text-->
 
