d0673d12bdb315e93ba3b8fcc8fdb1a74b45f05d
gunawan98 <deddygunawan98@gmail.com>
2021-08-24 12:45:34 +0700

    remove image gk perlu

--- resources/views/admin/includes/style.blade.php
+++ resources/views/admin/includes/style.blade.php
@@ -1,6 +1,3 @@
-<link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
-<link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
