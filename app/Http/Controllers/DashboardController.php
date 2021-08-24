0c78f68e9a07b42b4d29932be8d766e383d121af
hafet17 <do.crazy192@gmail.com>
2021-08-24 18:33:50 +0700

    add kursus_unit & unit_month

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,12 +6,23 @@ use Illuminate\Http\Request;
 use App\Kursus;
 use App\Unit;
 use App\Komentar;
+use App\KursusUnit;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
 
     public function index()
     {
+
+        $kursus_unit = KursusUnit::with(['kursus', 'unit'])
+            ->groupBy('unit_id')->get();
+
+        $unit_month = Unit::select(DB::raw("COUNT(*) as count"))
+            ->whereMonth('created_at', date('Y'))
+            ->groupBy(DB::raw("Month(created_at)"))
+            ->pluck('count');
+
         return view(
             'admin.dashboard.index',
             [
@@ -19,6 +30,8 @@ class DashboardController extends Controller
                 'unit' => Unit::where('status', '1')->count(),
                 'pendaftar' => Unit::where('status', '0')->count(),
                 'komentar' => Komentar::all()->count(),
+                'kursus_unit' => $kursus_unit,
+                'unit_chart' => $unit_month,
             ]
         );
     }
