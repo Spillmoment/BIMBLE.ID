7e843329c0290a7ebca70c3ef318f9ba0187d93b
hafet17 <do.crazy192@gmail.com>
2021-08-27 14:04:40 +0700

    fix kursus chart

--- resources/views/admin/dashboard/chart/kursus.blade.php
+++ resources/views/admin/dashboard/chart/kursus.blade.php
@@ -86,37 +86,32 @@
         }
     });
 
-     //pie chart
-     var ctx = document.getElementById("pieChart");
-    ctx.height = 300;
+    // single bar chart
+    var ctx = document.getElementById("singelBarChart");
+    ctx.height = 150;
     var myChart = new Chart(ctx,{
-        type: 'pie',
+        type: 'bar',
         data: {
-            datasets: [{
-                data: [45, 25, 20, 10],
-                backgroundColor: [
-                                    "rgba(0, 194, 146,0.9)",
-                                    "rgba(0, 194, 146,0.7)",
-                                    "rgba(0, 194, 146,0.5)",
-                                    "rgba(0,0,0,0.07)"
-                                ],
-                hoverBackgroundColor: [
-                                    "rgba(0, 194, 146,0.9)",
-                                    "rgba(0, 194, 146,0.7)",
-                                    "rgba(0, 194, 146,0.5)",
-                                    "rgba(0,0,0,0.07)"
-                                ]
-                            }],
-            labels:[
-                            "green",
-                            "green",
-                            "green"
+            labels: ["Sun", "Mon", "Tu", "Wed", "Th", "Fri", "Sat"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    data: [ 55, 50, 75, 80, 56, 55, 60 ],
+                    borderColor: "rgba(0, 194, 146, 0.9)",
+                    borderWidth: "0",
+                    backgroundColor: "rgba(0, 194, 146, 0.5)"
+                            }
                         ]
         },
         options: {
-            responsive: true
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                                }]
+            }
         }
-    });
     
   });
 </script>
