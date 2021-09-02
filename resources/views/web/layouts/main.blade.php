0df089c1200841f2f83957fdb943eea3d3853c58
gunawan98 <deddygunawan98@gmail.com>
2021-09-02 23:12:51 +0700

    ubah posisi include style front template

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -6,15 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="{{asset('assets/frontend/img/favicon.png') }}" type="image/x-icon">
+    @yield('meta')
     <title>@yield('title')</title>
+    @include('web.layouts.style')
+
     @stack('style')
 </head>
 
 <body style="padding-top: 72px;">
 
-    @include('web.layouts.style')
-
-    @stack('style')
+    
 
     @include('web.layouts.header')
 
