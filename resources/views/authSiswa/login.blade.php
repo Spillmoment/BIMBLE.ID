5bd3212c76388f401c8b4ab22aefe7b20e379cc1
hafet17 <do.crazy192@gmail.com>
2021-09-20 12:49:46 +0700

    re-layouting login user

--- resources/views/authSiswa/login.blade.php
+++ resources/views/authSiswa/login.blade.php
@@ -1,83 +1,70 @@
-<!-- Google fonts - Playfair Display-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
-<!-- Google fonts - Poppins-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
-<!-- Magnigic Popup-->
-<link rel="stylesheet" href="{{asset('assets/frontend/vendor/magnific-popup/magnific-popup.css') }}">
-<!-- theme stylesheet-->
-<link rel="stylesheet" href="{{asset('assets/frontend/vendor/bootstrap/style.default.css') }}" id="theme-stylesheet">
-<!-- Custom stylesheet - for your changes-->
-<link rel="stylesheet" href="{{asset('assets/frontend/css/custom.css') }}">
-<!-- Favicon-->
-<link rel="shortcut icon" href="{{asset('assets/frontend/img/favicon.png') }}">
+@include('web.layouts.style')
 
+@include('web.layouts.header')
+<title> Bimble | User Login </title>
 
-<title> Bimble | Siswa Login </title>
-<main class="login-container">
-    <div class="container">
-        <div class="row page-login d-flex justify-content-center">
-            <div class="section-left col-12 col-md-6">
-
-                <div class="card card-shadow mt-5" style="width: 30rem">
+<div class="container-fluid px-3 mt-5">
+    <div class="row min-vh-100 justify-content-center">
+        <div class="col-md-6 d-flex align-items-center">
+            <div class="w-100 py-5 px-md-5 px-xl-6 position-relative">
+                <div class="card shadow-lg mt-5">
                     <div class="card-body">
-                        <div class="text-center">
-                            <img src="{{ asset('assets/frontend/img/logo.png') }}" alt="" class="w-50 mb-2 mt-2" />
-                        </div>
                         <div class="text-center auth-logo-text">
-                            <h5 class="text-muted mb-4 mt-2">Siswa | Silahkan Login</h5>
-
-                            @if(session('loginError'))
-                            <div class="alert alert-warning" role="alert">
-                                <h6>{{ session('loginError') }}</h6>
-                            </div>
-                            @endif
-
+                            <h4 class="text-muted mb-4 mt-2">User | Silahkan Login</h4>
+                            <hr>
                         </div>
                         <!--end auth-logo-text-->
-
-                        <form method="post" action="{{ route('siswa.login.submit') }}">
+                   
+                        <form class="form-validate" method="POST" action="{{ route('siswa.login.submit') }}">
                             @csrf
-
                             <div class="form-group">
-                                <label class="form-label" for="email">Alamat Email</label>
+                                <label for="email" class="form-label">Email </label>
                                 <input id="email" type="email"
-                                    class="form-control form-input @error('email') is-invalid @enderror" name="email"
-                                    value="{{ old('email') }}" required autocomplete="email" autofocus
-                                    placeholder="Masukan Email">
-                                @error('email')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                                class="form-control form-input @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus
+                                placeholder="Masukan Email">
+                            @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
-
-                            <div class="form-group">
-                                <label class="form-label" for="password">Password</label>
+                            @enderror
+                            </div>
+                            <div class="form-group mb-4">
+                                <div class="row">
+                                    <div class="col">
+                                        <label for="password" class="form-label"> Password</label>
+                                    </div>
+                                    <div class="col-auto">
+                                        <a href="{{route('siswa.password.request') }}" class="float-right">
+                                           <small>Lupa Password?</small> </a>
+                                    </div>
+                                </div>
                                 <input id="password" type="password"
-                                    class="form-control form-input @error('password') is-invalid @enderror"
-                                    name="password" required autocomplete="current-password"
-                                    placeholder="Masukan Password" value="{{ old('password') }}">
-                                @error('password')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                                class="form-control form-input @error('password') is-invalid @enderror"
+                                name="password" required autocomplete="current-password"
+                                placeholder="Masukan Password" value="{{ old('password') }}">
+                            @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
                             </div>
-
-                            <div class="form-group form-check mb-5 mt-4">
-
-                                <a href="{{route('siswa.password.request') }}" class="float-right">
-                                    <i class="fas fa-lock mr-0"></i><small>Lupa Password?</small> </a>
+                            @enderror
                             </div>
-                            <button type="submit" class="btn btn-primary btn-block">
-                                <i class="fas fa-sign"></i>
-                                Login
-                            </button>
-
+                            <!-- Submit-->
+                            <button class="btn btn-lg btn-block btn-primary">Login</button>
+                            <hr class="my-4">
+                            <p class="text-center"><small class="text-muted text-center">Belum Punya akun bimble.id? <a
+                                        href="/siswa/register">Daftar Sekarang </a></small></p>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-lg-6 col-xl-7 d-none d-md-block">
+            <!-- Image-->
+            <div style="background-image: url(assets/img/photo/photo-1426122402199-be02db90eb90.jpg);"
+                class="bg-cover h-100 mr-n3"></div>
+        </div>
     </div>
-</main>
+</div>
+@include('web.layouts.script')
+@include('web.layouts.footer')
