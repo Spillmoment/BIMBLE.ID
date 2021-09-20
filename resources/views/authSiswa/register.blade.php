65552e70966c50c50e711ba6ffbdb3f6c508b4d3
hafet17 <do.crazy192@gmail.com>
2021-09-20 12:49:55 +0700

    re-layouting register user

--- resources/views/authSiswa/register.blade.php
+++ resources/views/authSiswa/register.blade.php
@@ -1,49 +1,27 @@
-<!-- Google fonts - Playfair Display-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
-<!-- Google fonts - Poppins-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
-<!-- Magnigic Popup-->
-<link rel="stylesheet" href="{{asset('assets/frontend/vendor/magnific-popup/magnific-popup.css') }}">
-<!-- theme stylesheet-->
-<link rel="stylesheet" href="{{asset('assets/frontend/vendor/bootstrap/style.default.css') }}" id="theme-stylesheet">
-<!-- Custom stylesheet - for your changes-->
-<link rel="stylesheet" href="{{asset('assets/frontend/css/custom.css') }}">
-<!-- Favicon-->
-<link rel="shortcut icon" href="{{asset('assets/frontend/img/favicon.png') }}">
+@include('web.layouts.style')
 
+@include('web.layouts.header')
+<title> Bimble | User Login </title>
 
-<title> Bimble | Siswa register </title>
-<main class="login-container">
-    <div class="container">
-        <div class="row page-login d-flex justify-content-center">
-            <div class="section-left col-12 col-md-6">
-
-                <div class="card card-shadow mt-5" style="width: 30rem">
+<div class="container-fluid px-3 mt-5">
+    <div class="row min-vh-100 justify-content-center">
+        <div class="col-md-7 d-flex align-items-center">
+            <div class="w-100 py-5 px-md-5 px-xl-6 position-relative">
+                <div class="card shadow-lg mt-5">
                     <div class="card-body">
-                        <div class="text-center">
-                            <img src="{{ asset('assets/frontend/img/logo.png') }}" alt="" class="w-50 mb-2 mt-2" />
-                        </div>
                         <div class="text-center auth-logo-text">
-                            <h5 class="text-muted mb-4 mt-2">Manager | Silahkan Login</h5>
-
-                            @if(session('loginError'))
-                            <div class="alert alert-warning" role="alert">
-                                <h6>{{ session('loginError') }}</h6>
-                            </div>
-                            @endif
-
-
+                            <h4 class="text-muted mb-4 mt-2">User | Silahkan Registrasi</h4>
+                            <hr>
                         </div>
                         <!--end auth-logo-text-->
-
-                        <form method="POST" action="{{ route('siswa.register.post') }}" enctype="multipart/form-data">
+                   
+                        <form class="form-validate" method="POST" action="{{ route('siswa.register.post') }}" enctype="multipart/form-data">
                             @csrf
-    
+                          
                             <div class="form-group">
-                                <label for="nama_siswa" class="col-form-label text-md-right">Nama Lengkap</label>
-    
-                                <input id="nama_siswa" type="text" class="form-control @error('nama_siswa') is-invalid @enderror" name="nama_siswa" value="{{ old('nama_siswa') }}" required autocomplete="nama_siswa">
-
+                                <label for="nama_siswa" class="form-label">Nama Lengkap</label>
+                                <input id="nama_siswa" type="text" class="form-control form-input @error('nama_siswa') is-invalid @enderror" name="nama_siswa" value="{{ old('nama_siswa') }}" required placeholder="Masukkan Nama Lengkap">
+                          
                                 @error('nama_siswa')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -52,15 +30,15 @@
                             </div>
                             
                             <div class="form-group">
-                                <label for="jenis_kelamin" class="col-form-label text-md-right">Jenis Kelamin</label> <br>
-    
+                                <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label> <br>
+                          
                                 <div class="form-check form-check-inline">
                                     <label for="jenis_kelamin">
-                                        <input type="radio" name="jenis_kelamin" value="L" id="jenis_kelamin" {{!old('jenis_kelamin') ? 'checked' : ''}} >Laki-Laki
-                                        <input type="radio" name="jenis_kelamin" value="P" id="jenis_kelamin" {{old('jenis_kelamin') ? 'checked' : ''}} >Perempuan
+                                        <input type="radio" name="jenis_kelamin" value="L" id="jenis_kelamin" {{!old('jenis_kelamin') ? 'checked' : ''}} > Laki-Laki
+                                        <input type="radio" name="jenis_kelamin" value="P" id="jenis_kelamin" {{old('jenis_kelamin') ? 'checked' : ''}} > Perempuan
                                     </label>
                                 </div>
-
+                          
                                 @error('jenis_kelamin')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -69,10 +47,10 @@
                             </div>
                             
                             <div class="form-group">
-                                <label for="agama" class="col-form-label text-md-right">Agama</label>
-    
-                                <input id="agama" type="text" class="form-control @error('agama') is-invalid @enderror" name="agama" value="{{ old('agama') }}" required autocomplete="agama">
-
+                                <label for="agama" class="form-label">Agama</label>
+                          
+                                <input id="agama" type="text" class="form-control @error('agama') is-invalid @enderror" name="agama" value="{{ old('agama') }}" required placeholder="Masukkan Agama">
+                          
                                 @error('agama')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -81,10 +59,10 @@
                             </div>
                             
                             <div class="form-group">
-                                <label for="alamat" class="col-form-label text-md-right">Alamat</label>
-    
-                                <input id="alamat" type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}" required autocomplete="alamat">
-
+                                <label for="alamat" class="form-label">Alamat</label>
+                          
+                                <input id="alamat" type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}" required placeholder="Masukkan Alamat">
+                          
                                 @error('alamat')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -93,72 +71,79 @@
                             </div>
                             
                             <div class="form-group">
-                                <label for="username" class="col-form-label text-md-right">Username</label>
-    
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username">
-
+                                <label for="username" class="form-label">Username</label>
+                          
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required placeholder="Masukkan Username">
+                          
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-    
+                          
                             <div class="form-group">
-                                <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-    
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                <label for="email" class="form-label">Alamat Email</label>
+                          
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required placeholder="Masukkan Alamat Email">
+                          
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-    
+                          
                             <div class="form-group">
-                                <label for="password" class="col-form-label text-md-right">{{ __('Password') }}</label>
-    
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <label for="password" class="form-label">Password</label>
+                          
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required placeholder="Masukkan Password">
+                          
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-    
+                          
                             <div class="form-group">
-                                <label for="password-confirm" class="col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-    
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <label for="password-confirm" class="form-label">Konfirmasi Password</label>
+                          
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required placeholder="Masukkan Konfirmasi Password">
                             </div>
-    
+                          
                             <div class="form-group">
-                                <label for="foto" class="col-form-label text-md-right">{{ __('foto') }}</label>
-    
+                                <label for="foto" class="form-label">{{ __('foto') }}</label>
+                          
                                 <input id="foto" type="file" class="form-control @error('foto') is-invalid @enderror" name="foto" required>
-
+                          
                                 @error('foto')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-    
+                          
                             <div class="form-group mb-0">
                                 <button type="submit" class="btn btn-primary btn-block">
-                                    {{ __('Register') }}
+                                    Daftar
                                 </button>
                             </div>
-                        </form>
-
-                        <div class="text-center">
-                            <a class="small" href="{{ route('siswa.login') }}">Sudah punya akun, silahkan login!</a>
-                        </div>
+                            <hr class="my-4">
+                            <p class="text-center"><small class="text-muted text-center">Sudah Punya akun bimble.id? <a
+                                        href="/siswa/login"><b>Login</b> </a></small></p>
+                          </form>
+                     
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-lg-6 col-xl-7 d-none d-md-block">
+            <!-- Image-->
+            <div style="background-image: url(assets/img/photo/photo-1426122402199-be02db90eb90.jpg);"
+                class="bg-cover h-100 mr-n3"></div>
+        </div>
     </div>
-</main>
+</div>
+@include('web.layouts.script')
+@include('web.layouts.footer')
