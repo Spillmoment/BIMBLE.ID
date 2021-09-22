14d756136e5f08c6ebc514cf039f92c61bd29e5c
hafet17 <do.crazy192@gmail.com>
2021-09-23 00:58:05 +0700

    refactor main template

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -12,11 +12,6 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
-        <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-        </div>
-
         <!-- Navbar -->
         @include('admin.layouts.navbar')
         <!-- /.navbar -->
