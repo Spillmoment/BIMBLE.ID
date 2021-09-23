49dd5c6d07e0bceaa2472966674b37cf0e904cc1
hafet17 <do.crazy192@gmail.com>
2021-09-23 11:54:28 +0700

    refactor galeri create

--- resources/views/admin/gallery/create.blade.php
+++ resources/views/admin/gallery/create.blade.php
@@ -1,67 +1,78 @@
-@extends('admin.layouts.manager')
+@extends('admin.layouts.main')
 
-@section('title','Bimble - Tambah Data Gallery')
+@section('title','Bimble - Tambah Galeri Kursus')
 @section('content')
-
-<div class="breadcrumbs">
-    <div class="breadcrumbs-inner">
-        <div class="row m-0">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Tambah Gallery Kursus</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="{{ route('gallery.index') }}">Data Gallery</a></li>
-                            <li class="active">Tambah Gallery </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">Tambah Galeri Kursus</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('kursus.index') }}">Kursus</a></li>
+                    <li class="breadcrumb-item active">Tambah Galeri Kursus </li>
+                </ol>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
 </div>
+<!-- /.content-header -->
 
-<div class="content">
-    <div class="card">
-        <div class="card-header">
-            <strong>Tambah Gallery</strong>
+<!-- Main content -->
+<section class="content">
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-12">
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Form Tambah Galeri Kursus</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form method="post" action="{{route('gallery.store')}}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="kursus_id">Kursus</label>
+                                <select class="form-control" name="kursus_id" id="kursus_id">
+                                    <option value=""> --- Pilih Kursus ---</option>
+
+                                    @foreach ($kursus as $kursus)
+                                    <option value="{{ $kursus->id }}">{{ $kursus->nama_kursus }}</option>
+                                    @endforeach
+
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="gambar">Gambar</label>
+                                <input type="file" class="form-control-file" name="gambar[]" id="gambar" multiple>
+                                <small class="text-muted">Upload Gambar Lebih Dari Satu</small>
+                            </div>
+
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-block" type="submit">
+                                    Simpan
+                                </button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+                <!-- /.card -->
+
+
+            </div>
+            <!-- /.col -->
         </div>
-        <div class="card-body card-block">
-            <form method="post" action="{{route('gallery.store')}}" enctype="multipart/form-data">
-                @csrf
 
-                <div class="form-group">
-                    <label for="kursus_id">Kursus</label>
-                    <select class="form-control" name="kursus_id" id="kursus_id">
-                        <option value=""> --- Pilih Kursus ---</option>
-
-                        @foreach ($kursus as $kursus)
-                        <option value="{{ $kursus->id }}">{{ $kursus->nama_kursus }}</option>
-                        @endforeach
-
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="gambar">Gambar</label>
-                    <input type="file" class="form-control-file" name="gambar[]" id="gambar" multiple="true">
-                    <small class="text-muted">Upload Gambar Lebih Dari Satu</small>
-                </div>
-
-                <div class="form-group">
-                    <button class="btn btn-primary btn-block" type="submit">
-                        Simpan
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
+        <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 @endsection
