10e7ca9ea349309c31bdabb93105c452268e56a6
gunawan98 <deddygunawan98@gmail.com>
2021-09-24 13:39:54 +0700

    blade profile user

--- resources/views/web/web_profile.blade.php
+++ resources/views/web/web_profile.blade.php
@@ -13,8 +13,8 @@
                 <div class="list-group">
                     <a href="{{ route('profile.index') }}"
                         class="{{ Route::currentRouteName() == 'profile.index' ? 'active' : '' }} list-group-item list-group-item-action">Profil</a>
-                    <a href=""
-                        class=" list-group-item list-group-item-action">Kursus</a>
+                    <a href="{{ route('user.kursus') }}"
+                        class="{{ Route::currentRouteName() == 'user.kursus' ? 'active' : '' }} list-group-item list-group-item-action">Kursus</a>
                     <a href=""
                         class=" list-group-item list-group-item-action">Sertifikat</a>
                     <a href="{{ route('profile.pengaturan') }}"
@@ -26,9 +26,11 @@
                 <div class="row">
                     <div class="col-md-12">
                         @if(Route::currentRouteName() == 'profile.index')
-                        @include('web.profile.profile')
-                        {{-- @elseif(Route::currentRouteName() == 'profile.kursus')
-                        @include('web.profile.kursus') --}}
+                            @include('web.profile.profile')
+                        @elseif(Route::currentRouteName() == 'user.kursus')
+                            @include('web.profile.kursus')
+                        @elseif(Route::currentRouteName() == 'user.materi')
+                            @include('web.profile.materi')
                         @elseif(Route::currentRouteName() == 'profile.pengaturan')
                         @include('web.profile.pengaturan')
                         @endif
