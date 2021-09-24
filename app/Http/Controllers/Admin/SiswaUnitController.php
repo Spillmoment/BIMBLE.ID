2688b6bff517855cf3ec038945211fd9b899a6b9
hafet17 <do.crazy192@gmail.com>
2021-09-24 13:33:03 +0700

    create SiswaUnitController

---
+++ app/Http/Controllers/Admin/SiswaUnitController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SiswaUnitController extends Controller
+{
+    public function index()
+    {
+    }
+}
