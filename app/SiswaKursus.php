e587ea6bd4c4c42e6104fe5db146a86de5581882
gunawan98 <deddygunawan98@gmail.com>
2021-09-24 13:43:35 +0700

    model siswaKursus

---
+++ app/SiswaKursus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SiswaKursus extends Model
+{
+    protected $table = 'siswa_kursus';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'siswa_id', 'kursus_unit_id', 'status_sertifikat', 'nilai', 'predikat', 'file'
+    ];
+    
+    public function siswa()
+    {
+        return $this->belongsTo(Siswa::class, 'siswa_id');
+    }
+    
+    public function kursus_unit()
+    {
+        return $this->belongsTo(KursusUnit::class, 'kursus_unit_id');
+    }
+}
