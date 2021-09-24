61c8bcb22655cf705b339630bf4edd2ea0eb4430
gunawan98 <deddygunawan98@gmail.com>
2021-09-24 13:44:14 +0700

    create model mentorKursus

---
+++ app/MentorKursus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MentorKursus extends Model
+{
+    protected $table = 'mentor_kursus';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'mentor_id', 'kursus_unit_id', 'pengalaman'
+    ];
+    
+    public function mentor()
+    {
+        return $this->belongsTo(Mentor::class, 'mentor_id');
+    }
+    
+    public function kursus_unit()
+    {
+        return $this->belongsTo(KursusUnit::class, 'kursus_unit_id');
+    }
+}
