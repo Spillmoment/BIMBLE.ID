791c23b6cafd0f96284308b973720253fe032282
gunawan98 <deddygunawan98@gmail.com>
2021-09-24 13:41:36 +0700

    create migrate materi table

---
+++ database/migrations/2021_09_21_074005_create_materi_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMateriTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materi', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('kursus_unit_id');
+            $table->foreign('kursus_unit_id')->references('id')->on('kursus_unit')->onDelete('cascade');
+
+            $table->integer('bab');
+            $table->string('judul');
+            $table->string('file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materi');
+    }
+}
