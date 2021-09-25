958f7b2f9d48dfb339584d8a5e9cd4f941afc0fe
hafet17 <do.crazy192@gmail.com>
2021-09-26 00:40:10 +0700

    create function in siswaunitcontroller

--- app/Http/Controllers/Admin/SiswaUnitController.php
+++ app/Http/Controllers/Admin/SiswaUnitController.php
@@ -4,10 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\SiswaKursus;
 
 class SiswaUnitController extends Controller
 {
     public function index()
     {
+        $siswa_unit = SiswaKursus::with(['siswa', 'kursus_unit'])
+            ->latest()->get();
+
+        return view('admin.siswa_unit.index', [
+            'unit' => $siswa_unit
+        ]);
+    }
+
+    public function detail_siswa($id)
+    {
+        $siswa = SiswaKursus::with(['siswa', 'kursus_unit'])
+            ->findOrFail($id);
+        dd($siswa);
     }
 }
