6d581fe7cf98cc04927ab77ef0e7caf0b65fe51d
hafet17 <do.crazy192@gmail.com>
2021-09-26 11:23:10 +0700

    refactor gallery kursus

--- resources/views/admin/kursus/gallery.blade.php
+++ resources/views/admin/kursus/gallery.blade.php
@@ -1,91 +1,73 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.app')
 
-@section('title','Bimble - Galeri Kursus')
+@section('title', 'Admin - Galeri Kursus')
+
 @section('content')
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Galeri Kursus {{ $kursus->nama_kursus }}</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('kursus.index') }}">Kursus</a></li>
-                    <li class="breadcrumb-item active">Galeri Kursus </li>
-                </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
 
-<!-- Main content -->
-<section class="content">
-
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h4 class="card-title">{{ $kursus->nama_kursus }}</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            @forelse ($items as $gallery)
-                            @foreach (explode('|', $gallery->gambar) as $image)
-                            <div class="col-sm-3">
-                                <a href="/storage/image/{{$image}}" data-toggle="lightbox"
-                                    data-title="Galeri - {{ $kursus->nama_kursus }}" data-gallery="gallery">
-                                    <img src="/storage/image/{{$image}}" class="img-fluid mb-2" alt="white sample" />
-                                </a>
-                            </div>
-
-                            @endforeach
-                            @empty
-                            <div class="col">
-                                <div
-                                    class="my-2 alert alert-warning col-lg-12 col-sm-12 col-md-12 text-center text-black font-weight-bold">
-                                    Galeri Kursus Kosong <a href="{{ route('kursus.index') }}"
-                                        class="btn btn-warning text-light btn-md">Kembali Ke Kursus</a>
-
-                                </div>
-                            </div>
-
-                            @endforelse
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- /.col -->
-        </div>
-
-        <!-- /.row (main row) -->
-    </div><!-- /.container-fluid -->
-</section>
-<!-- /.content -->
-@endsection
+@if (session('status'))
 @push('scripts')
 <script>
-    $(function () {
-        $(document).on('click', '[data-toggle="lightbox"]', function (event) {
-            event.preventDefault();
-            $(this).ekkoLightbox({
-                alwaysShowClose: true
-            });
-        });
-
-        $('.filter-container').filterizr({
-            gutterPixels: 3
-        });
-        $('.btn[data-filter]').on('click', function () {
-            $('.btn[data-filter]').removeClass('active');
-            $(this).addClass('active');
-        });
-    })
+    swal({
+        title: "Good job!",
+        text: "{{ session('status') }}",
+        icon: "success",
+        button: false,
+        timer: 3000
+    });
 
 </script>
 @endpush
+@endif
+
+<div class="row">
+    <div class="col-12 mb-4">
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+            <div class="d-block mb-4 mb-md-0">
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                    <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                        <li class="breadcrumb-item"><a href="#"><span class="fas fa-home"></span></a></li>
+                        <li class="breadcrumb-item"><a href="#">Kursus</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Galeri Kursus</li>
+                    </ol>
+                </nav>
+
+            </div>
+
+        </div>
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4 class="card-title">Galeri {{ $kursus->nama_kursus }}</h4>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @forelse ($items as $gallery)
+                    @foreach (explode('|', $gallery->gambar) as $image)
+                    <div class="col-sm-3">
+                        <a href="/storage/image/{{$image}}" data-toggle="lightbox"
+                            data-title="Galeri - {{ $kursus->nama_kursus }}" data-gallery="gallery">
+                            <img src="/storage/image/{{$image}}" class="img-fluid mb-2" alt="white sample" />
+                        </a>
+                    </div>
+
+                    @endforeach
+                    @empty
+                    <div class="col">
+                        <div
+                            class="alert alert-primary col-lg-12 col-sm-12 col-md-12 text-center text-light font-weight-bold">
+                            <big>Galeri Kursus Kosong</big> <a href="{{ route('kursus.index') }}"
+                                class="btn btn-info text-light btn-md">Kembali Ke Kursus</a>
+
+                        </div>
+                    </div>
+
+                    @endforelse
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
