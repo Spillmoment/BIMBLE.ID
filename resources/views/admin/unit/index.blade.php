74a855d7438737b5f1f732769ec03505861a9774
hafet17 <do.crazy192@gmail.com>
2021-09-27 08:29:29 +0700

    refactor views index unit

--- resources/views/admin/unit/index.blade.php
+++ resources/views/admin/unit/index.blade.php
@@ -1,135 +1,118 @@
-@extends('admin.layouts.main')
+@extends('admin.layouts.app')
 
-@section('title','Bimble - Halaman Unit')
+@section('title', 'Admin - Halaman Unit')
+
 @section('content')
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Halaman Unit</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Unit </li>
-                </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
 
-<!-- Main content -->
-<section class="content">
-
-    <div class="container-fluid">
-
-        @if(session('status'))
-        @push('scripts')
-        <script>
-            swal({
-                title: "Success",
-                text: "{{session('status')}}",
-                icon: "success",
-                button: false,
-                timer: 2000
-            });
-
-        </script>
-        @endpush
-        @endif
-
-        <div class="row">
-            <div class="col-12">
-
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Tabel Data Unit</h3>
-                        <a name="" id="" class="btn btn-primary float-right" href="{{ route('unit.create') }}"
-                            role="button"> <i class="fas fa-plus"></i> Tambah
-                            Unit</a>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
-                        <table id="example1" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th width="100">Nama Unit</th>
-                                    <th>Alamat</th>
-                                    <th>Deksripsi</th>
-                                    <th>Status</th>
-                                    <th width="100">Option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($unit as $u)
-
-                                <tr>
-                                    <td scope="row"> {{$loop->iteration}} </td>
-                                    <td>{{ $u->nama_unit }}</td>
-                                    <td> {{ $u->alamat }} </td>
-
-                                    <td>@if ($u->deskripsi != null)
-                                        {!! $u->deskripsi !!}
-                                        @else
-                                        Belum ada deskripsi
-                                        @endif
-                                    </td>
-
-                                    <td>
-                                        @if ($u->status === '1')
-                                        <span class="badge badge-pill badge-success">Aktif</span>
-                                        @else
-                                        <span class="badge badge-pill badge-danger">Tidak Aktif</span>
-                                        @endif
-                                    </td>
-
-                                    <td>
-                                        <a class="btn btn-info text-white btn-sm" href="{{route('unit.show',
-                                       [$u->id])}}"> <i class="fa fa-eye"></i></a>
-                                        <a class="btn btn-warning text-white btn-sm" href="{{route('unit.edit',
-                                          [$u->id])}}"> <i class="fa fa-edit"></i> </a>
-                                        <form class="d-inline" action="{{route('unit.destroy', [$u->id])}}"
-                                            method="POST">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" id="deleteButton" data-name="{{ $u->nama_unit }}"
-                                                class="btn btn-danger btn-sm">
-                                                <i class="fa fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-
-                                </tr>
-                                @endforeach
-                            </tbody>
-
-
-                        </table>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-            </div>
-            <!-- /.col -->
-        </div>
-
-        <!-- /.row (main row) -->
-    </div><!-- /.container-fluid -->
-</section>
-<!-- /.content -->
-@endsection
-
+@if (session('status'))
 @push('scripts')
 <script>
+    swal({
+        title: "Berhasil",
+        text: "{{ session('status') }}",
+        icon: "success",
+        button: false,
+        timer: 3000
+    });
+
+</script>
+@endpush
+@endif
+
+<div class="row">
+    <div class="col-12 mb-4">
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+            <div class="d-block mb-4 mb-md-0">
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                    <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                        <li class="breadcrumb-item"><a href="#"><span class="fas fa-home"></span></a></li>
+                        <li class="breadcrumb-item"><a href="#">Unit</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Halaman Unit</li>
+                    </ol>
+                </nav>
+                <h2 class="h4">Table Unit</h2>
+            </div>
+
+        </div>
+        <div class="card border-light shadow-sm components-section">
+            <div class="row">
+                <div class="card-body">
+                    <table class="table table-hover" id="unitTable">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Unit</th>
+                                <th>Email</th>
+                                <th>Alamat</th>
+                                <th>Gambar Unit</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                    <footer class="footer section py-2">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@push('scripts')
+<script>
+    // AJAX DataTable
+    var datatable = $('#unitTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        ajax: {
+            url: '{!! url()->current() !!}',
+        },
+        columns: [{
+                "data": 'id',
+                "sortable": false,
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            {
+                data: 'nama_unit',
+                name: 'nama_unit'
+            },
+            {
+                data: 'email',
+                name: 'email'
+            },
+            {
+                data: 'alamat',
+                name: 'alamat'
+            },
+            {
+                data: 'gambar_unit',
+                name: 'gambar_unit'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false,
+                width: '20%'
+            },
+        ],
+
+    });
+
     $('button#deleteButton').on('click', function (e) {
         var name = $(this).data('name');
         e.preventDefault();
         swal({
                 title: "Yakin!",
-                text: "Menghapus Unit  " + name + "?",
+                text: "menghapus kursus  " + name + "?",
                 icon: "warning",
                 dangerMode: true,
                 buttons: {
