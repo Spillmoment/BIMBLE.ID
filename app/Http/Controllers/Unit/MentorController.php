6bd172b54915ec1ce97143fd08c9d54979b76d46
gunawan98 <deddygunawan98@gmail.com>
2021-09-28 20:05:12 +0700

    mentor controller

--- app/Http/Controllers/Unit/MentorController.php
+++ app/Http/Controllers/Unit/MentorController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Unit;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MentorRequest;
+use App\KursusUnit;
 use App\Mentor;
+use App\MentorKursus;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
@@ -76,5 +78,92 @@ class MentorController extends Controller
         $mentor->forceDelete();
         return redirect()->route('mentor.index')
             ->with(['status' => 'Data mentor Berhasil Dihapus']);
+    }
+
+    public function penempatan()
+    {
+        return view('unit.mentor.penempatan_index', [
+            'mentor' => MentorKursus::with(['mentor','kursus_unit'])
+                                    ->whereHas('kursus_unit', function ($q) {
+                                        $q->where('unit_id', Auth::id());
+                                    })
+                                    ->get()
+        ]);
+    }
+
+    public function create_penempatan()
+    {
+        $mentor = Mentor::where('unit_id', Auth::id())->get();
+        $kursus_unit = KursusUnit::with('kursus')->where('unit_id',  Auth::id())->get();
+        return view('unit.mentor.penempatan_create', compact('mentor','kursus_unit'));
+    }
+
+    public function store_penempatan(Request $request)
+    {
+        $request->validate([
+            'mentor_id' => 'required|numeric' ,
+            'kursus_unit_id' => 'required|numeric' ,
+            'pengalaman' => 'required'
+        ]);
+        
+        $cek_data = MentorKursus::where('mentor_id', $request->mentor_id)->where('kursus_unit_id', $request->kursus_unit_id)->first();
+
+        if ($cek_data) {
+            return redirect()->back()->with(['warning' => 'Mentor dan kursus tersebut sudah ada sebelumnya.']);
+        } else {
+            MentorKursus::create([
+                'mentor_id' => $request->mentor_id,
+                'kursus_unit_id' => $request->kursus_unit_id,
+                'pengalaman' => $request->pengalaman
+            ]);
+    
+            return redirect()->route('penempatan.index')
+                ->with(['status' => 'Data Mentor Berhasil Ditambahkan']);
+        }
+        
+    }
+
+    public function edit_penempatan(MentorKursus $id)
+    {
+        $mentor = Mentor::where('unit_id', Auth::id())->get();
+        $kursus_unit = KursusUnit::with('kursus')->where('unit_id',  Auth::id())->get();
+        return view('unit.mentor.penempatan_edit', [
+            'mentor_kursus' => $id,
+            'mentor' => $mentor,
+            'kursus_unit' => $kursus_unit
+        ]);
+    }
+
+    public function update_penempatan(Request $request, $id)
+    {
+        $request->validate([
+            'mentor_id' => 'required|numeric' ,
+            'kursus_unit_id' => 'required|numeric' ,
+            'pengalaman' => 'required'
+        ]);
+        
+        $cek_data = MentorKursus::where('mentor_id', $request->mentor_id)
+                                ->where('kursus_unit_id', $request->kursus_unit_id)
+                                ->where('id', '!=', $id)
+                                ->first();
+
+        if ($cek_data) {
+            return redirect()->back()->with(['warning' => 'Mentor dan kursus tersebut sudah ada sebelumnya.']);
+        } else {
+            $mentor_kursus = MentorKursus::find($id);
+            $data = $request->all();
+            $mentor_kursus->update($data);
+    
+            return redirect()->route('penempatan.index')
+                ->with(['status' => 'Data Mentor Berhasil Diupdate.']);
+        }
+    }
+
+    public function delete_penempatan($id)
+    {
+        $mentor_kursus = MentorKursus::find($id);
+        $mentor_kursus->delete();
+        return redirect()->route('penempatan.index')
+            ->with(['status' => 'Data penempatan mentor berhasil dihapus']);
     }
 }
