a6be114059867b4c950dea04bb74994b5feb3dd6
hafet17 <do.crazy192@gmail.com>
2021-09-28 23:44:21 +0700

    refactor views email manager

--- resources/views/authManager/passwords/email.blade.php
+++ resources/views/authManager/passwords/email.blade.php
@@ -1,49 +1,55 @@
-@include('layouts.style')
+@include('admin.layouts.style')
+<title> Bimble | Reset Password</title>
 
-<title> Manager | Reset Password </title>
-<main class="login-container">
-    <div class="container">
-        
-        <div class="row page-login d-flex justify-content-center">
-            <div class="section-left col-12 col-md-6">
-               <div class="card card-shadow mt-5" style="width: 30rem;">
-                    <div class="card-body">
-                        
-                        @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<body class="bg-soft">
+    <main>
 
-                        <div class="text-center">
-                            <img src="{{ asset('assets/frontend/img/logo.png') }}" alt="" class="w-50 mb-2 mt-2" />
-                        </div>
-                        <div class="text-center auth-logo-text">
-                            <h5 class="text-muted mb-4 mt-2">Masukkan Email Untuk Verifikasi</h5>  
-                        </div> <!--end auth-logo-text-->  
-                        <form method="POST" action="{{ route('manager.password.email') }}">
-                         @csrf                           
-                             <div class="form-group">
-                                <label class="form-label" for="email">Alamat Email</label>
-                                <input id="email" type="email" class="form-control form-input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="Masukkan Email">
-                            
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror     
+        <!-- Section -->
+        <section class="vh-lg-100 bg-soft d-flex align-items-center">
+            <div class="container">
+                <div class="row justify-content-center form-bg-image">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <div
+                            class="signin-inner my-3 my-lg-0 bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
+                            <h1 class="h3">Masukkan Email Untuk Reset Password</h1>
+                            @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
                             </div>
-                            
-                            <button type="submit" class="btn btn-primary btn-block">
-                                <i class="fas fa-sign-in-alt mr-2"></i>
-                                Kirim Email
-                            </button>
-                           
-                        </form>
+                            @endif
+                            <form method="POST" action="{{ route('manager.password.email') }}">
+                                @csrf
+                                <!-- Form -->
+                                <div class="mb-4">
+                                    <label for="email">Alamat Email</label>
+                                    <div class="input-group">
+                                        <input type="email"
+                                            class="form-control form-input @error('email') is-invalid @enderror"
+                                            name="email" value="{{ old('email') }}" id="email" required autofocus
+                                            placeholder="Masukkan Email">
+                                    </div>
+                                    @error('email')
+                                    <div class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </div>
+                                    @enderror
+                                </div>
+                                <!-- End of Form -->
+                                <button type="submit" class="btn btn-block btn-primary">Kirim Email</button>
+                            </form>
+                            <div class="d-flex justify-content-center align-items-center mt-4">
+                                <span class="font-weight-normal">
+                                    Kembali ke
+                                    <a href="{{ route('manager.login') }}" class="font-weight-bold">Halaman Login</a>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
- 
-    </div>
-  </main>
+        </section>
+    </main>
+
+    @include('admin.layouts.script')
+
+</body>
