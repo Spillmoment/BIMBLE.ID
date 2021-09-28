922baae4fcbf6d01dd5945951bb59652db2a9961
hafet17 <do.crazy192@gmail.com>
2021-09-28 21:01:41 +0700

    implement datatable in view komentar

--- resources/views/admin/komentar/index.blade.php
+++ resources/views/admin/komentar/index.blade.php
@@ -1,128 +1,113 @@
-@extends('admin.layouts.manager')
-@section('title','Bimble - Data Review Kursus')
+@extends('admin.layouts.app')
 
+@section('title', 'Admin - Halaman Komentar Kursus')
 
 @section('content')
 
-<div class="breadcrumbs">
-    <div class="breadcrumbs-inner">
-        <div class="row m-0">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Data Review Kursus</h1>
-                    </div>
-                </div>
+@if (session('status'))
+@push('scripts')
+<script>
+    swal({
+        title: "Berhasil",
+        text: "{{ session('status') }}",
+        icon: "success",
+        button: false,
+        timer: 3000
+    });
+
+</script>
+@endpush
+@endif
+
+<div class="row">
+    <div class="col-12 mb-4">
+
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+            <div class="d-block mb-4 mb-md-0">
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                    <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                        <li class="breadcrumb-item"><a href="#"><span class="fas fa-home"></span></a></li>
+                        <li class="breadcrumb-item"><a href="#">Kursus</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Halaman Komentar Kursus</li>
+                    </ol>
+                </nav>
+                <h2 class="h4">Table Komentar Kursus</h2>
             </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="{{ route('unit.index') }}">Data Review Kursus</a></li>
-                            <li class="active">List Review Kursus </li>
-                        </ol>
-                    </div>
+
+        </div>
+        <div class="card border-light shadow-sm components-section">
+            <div class="row">
+                <div class="card-body">
+                    <table class="table table-hover" id="komentarTable">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Kursus</th>
+                                <th>Unit</th>
+                                <th>Option</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                    <footer class="footer section py-2">
+
                 </div>
+
             </div>
         </div>
     </div>
 </div>
 
-@push('after-script')
-@if(session('status'))
-<script>
-    swal({
-        title: "Success",
-        text: "{{session('status')}}",
-        icon: "success",
-        button: false,
-        timer: 2000
-    });
-</script>
-@elseif(session('success'))
-<script>
-    swal({
-        title: "Success",
-        text: "{{session('success')}}",
-        icon: "success",
-        timer: 2000
-    });
-</script>
-@endif
-@endpush
-
-<div class="content">
-    <div class="animated fadeIn">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <strong class="card-title">Table Review Kursus</strong>
-                    </div>
-                    <div class="card-body">
-                        <table id="bootstrap-data-table" class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th width="100">Nama</th>
-                                    <th width="100">Email</th>
-                                    <th>Komentar</th>
-                                    <th>Kursus</th>
-                                    <th>Unit</th>
-                                    <th width="100">Option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                              
-                              @foreach ($komentar as $r)
-                              <tr>
-                                    <td scope="row"> {{$loop->iteration}} </td>
-
-                                    <td>{{ $r->nama }}</td>
-                                    <td> {{ $r->email }} </td>
-                                    <td>{{ $r->komentar }}</td>
-                                    
-                                    @foreach ($r->kursus_unit as $item)  
-                                    <td> {{ $item->kursus->nama_kursus }} </td>
-                                    <td> {{ $item->unit->nama_unit }} </td>
-                                    @endforeach
-                                  
-                                    <td>
-                                        <form class="d-inline" action="{{route('komentar.destroy', [$r->id])}}"
-                                            method="POST">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" id="deleteButton" data-name="{{ $item->kursus->nama_kursus }}"
-                                                class="btn btn-danger btn-sm">
-                                                <i class="fa fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-
-                                  </tr>
-                                  @endforeach
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-    </div><!-- .animated -->
-</div>
 @endsection
-
-@push('after-script')
-@include('admin.includes.datatable')
+@push('scripts')
 <script>
+    // AJAX DataTable
+    var datatable = $('#komentarTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        ajax: {
+            url: '{!! url()->current() !!}',
+        },
+        columns: [{
+                "data": 'id',
+                "sortable": false,
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            {
+                data: 'nama',
+                name: 'nama'
+            },
+            {
+                data: 'kursus',
+                name: 'kursus.nama_kursus'
+            },
+            {
+                data: 'unit',
+                name: 'unit.nama_unit'
+            },
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false,
+                width: '20%'
+            },
+        ],
+
+    });
+
     $('button#deleteButton').on('click', function (e) {
         var name = $(this).data('name');
         e.preventDefault();
         swal({
                 title: "Yakin!",
-                text: "Menghapus review kursus  " + name + "?",
+                text: "menghapus komentar " + name + "?",
                 icon: "warning",
                 dangerMode: true,
                 buttons: {
