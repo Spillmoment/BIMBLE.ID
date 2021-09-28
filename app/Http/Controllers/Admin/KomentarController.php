376258f62b290814ccf7b92527f14288adc9cc16
hafet17 <do.crazy192@gmail.com>
2021-09-28 20:59:46 +0700

    implement datatable komentar

--- app/Http/Controllers/Admin/KomentarController.php
+++ app/Http/Controllers/Admin/KomentarController.php
@@ -5,15 +5,53 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Komentar;
-
+use Yajra\DataTables\Facades\DataTables;
 
 class KomentarController extends Controller
 {
     public function index()
     {
-        return view('admin.komentar.index', [
-            'komentar' => Komentar::with('kursus_unit')->latest()->get()
-        ]);
+        if (request()->ajax()) {
+            $query = Komentar::query()->with(['kursus_unit'])
+                ->latest();
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return
+                        '<div class="btn-group">
+                            <button class="btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                                <span class="icon icon-sm">
+                                    <span class="fas fa-ellipsis-h icon-dark"></span>
+                                </span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                <a class="dropdown-item" href="' . route('komentar.show', $item->id) . '"><span
+                                class="fas fa-eye mr-2"></span>Detail</a>  
+                                 <form action="' . route('komentar.destroy', $item->id) . '" method="POST">
+                                    ' . method_field('delete') . csrf_field() . '
+                                    <button id="deleteButton" type="submit" class="dropdown-item text-danger" data-name="' . $item->nama .  '">
+                                        <span class="fas fa-trash-alt mr-2"></span>Hapus</a>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>';
+                })
+                ->addColumn('kursus', function ($item) {
+                    return $item->kursus_unit->map(function ($kur) {
+                        return $kur->kursus->nama_kursus;
+                    })->implode("");
+                })
+                ->addColumn('unit', function ($item) {
+                    return $item->kursus_unit->map(function ($kur) {
+                        return $kur->unit->nama_unit;
+                    })->implode("");
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+
+        return view('admin.komentar.index');
     }
 
 
@@ -30,7 +68,9 @@ class KomentarController extends Controller
 
     public function show($id)
     {
-        //
+        return view('admin.komentar.show', [
+            'komentar' => Komentar::findOrFail($id)
+        ]);
     }
 
 
