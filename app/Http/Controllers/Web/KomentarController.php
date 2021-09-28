687ef9c680876c1e5013b0ef7179d73129a4451e
hafet17 <do.crazy192@gmail.com>
2021-09-28 21:00:30 +0700

    change query create komentar

--- app/Http/Controllers/Web/KomentarController.php
+++ app/Http/Controllers/Web/KomentarController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\KursusUnit;
 use App\Komentar;
-use App\Unit;
 use App\Http\Requests\KomentarRequest;
 
 class KomentarController extends Controller
@@ -14,14 +12,13 @@ class KomentarController extends Controller
 
     public function post(KomentarRequest $request, $id)
     {
-        $kursus_unit = KursusUnit::where('id', $id)
-            ->first()->id;
+        Komentar::create([
+            'nama' => auth()->user()->nama_siswa,
+            'email' => auth()->user()->email,
+            'kursus_unit_id' =>  KursusUnit::where('id', $id)->first()->id,
+            'komentar' => $request->komentar
+        ]);
 
-        $komentar = $request->all();
-        $komentar['kursus_unit_id'] = $kursus_unit;
-
-        Komentar::create($komentar);
-
-        return redirect()->back()->with(['message' => 'Review berhasil dikirim']);
+        return back()->with(['message' => 'Review berhasil dikirim']);
     }
 }
