b54c783155d8f04eec593afbc80e69b2fb5058da
gunawan98 <deddygunawan98@gmail.com>
2021-09-30 21:53:19 +0700

    model mentor kursus

--- app/MentorKursus.php
+++ app/MentorKursus.php
@@ -9,7 +9,7 @@ class MentorKursus extends Model
     protected $table = 'mentor_kursus';
     protected $primaryKey = 'id';
     protected $fillable = [
-        'mentor_id', 'kursus_unit_id', 'pengalaman'
+        'mentor_id', 'kursus_unit_id'
     ];
     
     public function mentor()
