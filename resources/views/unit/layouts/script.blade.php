8f9488a5116b1fd578cfd73dc670d9f120760730
hafet17 <do.crazy192@gmail.com>
2021-09-30 12:37:07 +0700

    wes

---
+++ resources/views/unit/layouts/script.blade.php
@@ -0,0 +1,42 @@
+<!-- Core -->
+<script src="{{ asset('assets/backend/vendor/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('assets/backend/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+<!-- Vendor JS -->
+<script src="{{ asset('assets/backend/vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
+
+<!-- JQuery JS -->
+<script src="{{ asset('assets/backend/vendor/jquery/jquery-3.5.1.min.js') }}"></script>
+
+<!-- Slider -->
+<script src="{{ asset('assets/backend/vendor/nouislider/distribute/nouislider.min.js') }}"></script>
+
+<!-- Jarallax -->
+<script src="{{ asset('assets/backend/vendor/jarallax/dist/jarallax.min.js') }}"></script>
+
+<!-- Smooth scroll -->
+<script src="{{ asset('assets/backend/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+
+<!-- Count up -->
+<script src="{{ asset('assets/backend/vendor/countup.js/dist/countUp.umd.js') }}"></script>
+
+<!-- Notyf -->
+<script src="{{ asset('assets/backend/vendor/notyf/notyf.min.js') }}"></script>
+
+<!-- Charts -->
+<script src="{{ asset('assets/backend/vendor/chartist/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('assets/backend/vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}">
+</script>
+
+<!-- Datepicker -->
+<script src="{{ asset('assets/backend/vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+<!-- Simplebar -->
+<script src="{{ asset('assets/backend/vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js')"></script>
+
+<!-- Volt JS -->
+<script src="{{ asset('assets/backend/js/volt.js') }}"></script>
+
---
+++ resources/views/unit/layouts/sidebar.blade.php
@@ -0,0 +1,88 @@
+<nav id="sidebarMenu" class="sidebar d-md-block bg-primary text-white collapse" data-simplebar>
+  <div class="sidebar-inner px-4 pt-3">
+      <div
+          class="user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4">
+
+          <div class="d-flex align-items-center">
+              <div class="user-avatar lg-avatar mr-4">
+                  <img src="{{ auth()->user()->gambar_unit 
+                  ? url('assets/images/unit/'. Auth::user()->gambar_unit) 
+                  : url('assets/images/unit/profile.png')
+                  }}"
+                      class="card-img-top rounded-circle border-white" alt="Bonnie Green">
+              </div>
+              <div class="d-block">
+                  <h2 class="h6">Hi, {{ auth()->user()->nama_unit }}</h2>
+                  <a href="{{ route('unit.logout') }}" onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();" class="btn btn-secondary text-dark btn-xs"><span
+                          class="mr-2"><span class="fas fa-sign-out-alt"></span></span>Sign Out</a>
+              </div>
+              <form id="logout-form" action="{{ route('unit.logout') }}" method="POST" style="display: none;">
+                  @csrf
+                  @method('HEAD')
+              </form>
+          </div>
+        
+
+
+          <div class="collapse-close d-md-none">
+              <a href="#sidebarMenu" class="fas fa-times" data-toggle="collapse" data-target="#sidebarMenu"
+                  aria-controls="sidebarMenu" aria-expanded="true" aria-label="Toggle navigation"></a>
+          </div>
+      </div>
+      <ul class="nav flex-column">
+
+  
+          <!-- Sidebar Unit -->
+          <li class="nav-item {{ Request::route()->getName() == 'unit.home' ? 'active' : '' }}">
+              <a href="{{ route('unit.home') }}" class="nav-link">
+                  <span class="sidebar-icon"><span class="fas fa-chart-pie"></span></span>
+                  <span>Dashboard</span>
+              </a>
+          </li>
+          <li class="nav-item {{ Request::route()->getName() == 'unit.kursus.home' ? 'active' : '' }}">
+              <a href="{{ route('unit.kursus.home') }}" class="nav-link">
+                  <span class="sidebar-icon"><span class="fas fa-book"></span></span>
+                  <span>Kursus</span>
+              </a>
+          </li>
+          <li class="nav-item {{ Request::route()->getName() == 'mentor.index' ? 'active' : '' }}">
+              <a href="{{ route('mentor.index') }}" class="nav-link">
+                  <span class="sidebar-icon"><span class="fas fa-users"></span></span>
+                  <span>Mentor</span>
+              </a>
+          </li>
+          <li class="nav-item {{ Request::route()->getName() == 'unit.fasilitas.home' ? 'active' : '' }}">
+              <a href="{{ route('unit.fasilitas.home') }}" class="nav-link">
+                  <span class="sidebar-icon"><span class="fas fa-building"></span></span>
+                  <span>Fasilitas</span>
+              </a>
+          </li>
+          <li
+          class="nav-item {{ (request()->is('unit/siswa/*')) ? 'active' : '' }}">
+          <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
+              data-toggle="collapse" data-target="#submenu-siswa">
+              <span>
+                  <span class="sidebar-icon"><span class="fas fa-user-circle"></span></span>
+                  Siswa Kursus
+              </span>
+              <span class="link-arrow"><span class="fas fa-chevron-right"></span></span>
+          </span>
+          <div class="multi-level collapse {{ (request()->is('unit/siswa/*')) || request()->is('manager/pendaftar-unit*') || request()->is('manager/siswa-unit*') ? 'show' : '' }}"
+              role="list" id="submenu-siswa" aria-expanded="false">
+              <ul class="flex-column nav">
+                  <li class="nav-item {{ (Request::route()->getName() == 'unit.siswa.kelompok') ? 'active' : '' }}">
+                      <a class="nav-link" href="{{ route('unit.siswa.kelompok') }}"><span>Kursus Kelompok</span></a>
+                  </li>
+                  <li class="nav-item {{ (Request::route()->getName() == 'unit.siswa.private') ? 'active' : '' }}">
+                      <a class="nav-link" href="{{ route('unit.siswa.private') }}"><span>Kursus Private</span></a>
+                  </li>
+              </ul>
+          </div>
+      </li>
+
+
+
+      </ul>
+  </div>
+</nav>
