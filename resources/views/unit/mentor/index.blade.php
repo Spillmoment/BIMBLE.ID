3436ab291e0c5482a39b2f0af90b64e67cd17ae0
gunawan98 <deddygunawan98@gmail.com>
2021-09-30 19:52:30 +0700

    index blade mentor di unit

--- resources/views/unit/mentor/index.blade.php
+++ resources/views/unit/mentor/index.blade.php
@@ -1,117 +1,109 @@
-@extends('admin.layouts.tutor')
+@extends('unit.layouts.app')
 
-@section('title','Unit - Halaman Mentor')
+@section('title', 'Unit - Halaman Mentor')
 @section('content')
 
-<div class="breadcrumbs">
-    <div class="breadcrumbs-inner">
-        <div class="row m-0">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Data Mentor</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="{{ route('mentor.index') }}">Data Mentor</a></li>
-                            <li class="active">List Mentor </li>
+    @if(session('status'))
+    @push('after-script')
+    <script>
+        swal({
+            title: "Success",
+            text: "{{session('status')}}",
+            icon: "success",
+            button: false,
+            timer: 2000
+        });
+
+    </script>
+    @endpush
+    @endif
+
+    <div class="row">
+        <div class="col-12 mb-4">
+    
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+                <div class="d-block mb-4 mb-md-0">
+                    <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+                        <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+                            <li class="breadcrumb-item"><a href="#"><span class="fas fa-home"></span></a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('mentor.index') }}">Data Mentor</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">List Mentor</li>
                         </ol>
+                    </nav>
+                    <h2 class="h4">Table Mentor</h2>
+                    <a class="btn btn-primary btn-sm float-right" href="{{ route('mentor.create') }}"> Tambah
+                        Mentor</a>
+                </div>
+    
+            </div>
+            <div class="card border-light shadow-sm components-section">
+                <div class="row">
+                    <div class="card-body">
+                        <table class="table table-hover" id="mentorTable">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Mentor</th>
+                                    <th>Foto</th>
+                                    <th>Kompetensi</th>
+                                    <th width="210">Opsi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+    
+                            </tbody>
+                        </table>
+                        <footer class="footer section py-2">
+    
                     </div>
+    
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-@if(session('status'))
-@push('after-script')
-<script>
-    swal({
-        title: "Success",
-        text: "{{session('status')}}",
-        icon: "success",
-        button: false,
-        timer: 2000
-    });
-
-</script>
-@endpush
-@endif
-
-<div class="content">
-    <div class="animated fadeIn">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <strong class="card-title">Table Mentor</strong>
-
-                        <a class="btn btn-primary btn-sm float-right" href="{{ route('mentor.create') }}"> Tambah
-                            Mentor</a>
-                    </div>
-                    <div class="card-body">
-                        <table id="bootstrap-data-table" class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th width="300">Nama</th>
-                                    <th width="150">Foto</th>
-                                    <th width="200">Kompetensi</th>
-                                    <th width="210">Option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($mentor as $mentor)
-                                <tr>
-                                    <td scope="row"> {{$loop->iteration}} </td>
-                                    <td>{{ $mentor->nama_mentor }}</td>
-                                    @if($mentor->foto)
-                                    <td> <img src="{{ Storage::url('public/'.$mentor->foto) }}" width="100px">
-                                    </td>
-                                    @else
-                                    Tidak Ada Gambar
-                                    @endif
-                                    <td>
-                                        {{ $mentor->kompetensi }}
-                                    </td>
-                                    <td>
-                                        <a class="btn btn-warning btn-sm text-light" href="{{route('mentor.edit',
-                                       [$mentor->id])}}"> <i class="fa fa-pencil"></i></a>
-
-                                        <form class="d-inline" action="{{route('mentor.destroy', [$mentor->id])}}"
-                                            method="POST">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" id="deleteButton" data-name="{{ $mentor->nama_mentor }}"
-                                                class="btn btn-danger btn-sm delete">
-                                                <i class="fa fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-
-                                </tr>
-                                @endforeach
-                            </tbody>
-
-                        </table>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-    </div><!-- .animated -->
-</div>
 
 @endsection
 
-@push('after-script')
-@include('admin.includes.datatable')
+@push('scripts')
 <script>
+    // AJAX DataTable
+    var datatable = $('#mentorTable').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: true,
+        ajax: {
+            url: '{!! url()->current() !!}',
+        },
+        columns: [{
+                "data": 'id',
+                "sortable": false,
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            {
+                data: 'nama_mentor',
+                name: 'nama_mentor'
+            },
+            {
+                data: 'gambar_mentor',
+                name: 'gambar_mentor'
+            },
+            {
+                data: 'kompetensi',
+                name: 'kompetensi'
+            },
+            {
+                data: 'option',
+                name: 'option',
+                orderable: false,
+                searchable: false,
+                width: '20%'
+            },
+        ],
+
+    });
+
     $('button#deleteButton').on('click', function (e) {
         var name = $(this).data('name');
         e.preventDefault();
