aac56ecafcbe4ce0f4d998b4e2d5c1c7641befe7
hafet17 <do.crazy192@gmail.com>
2021-09-30 12:36:56 +0700

    wes

---
+++ resources/views/unit/layouts/style.blade.php
@@ -0,0 +1,19 @@
+    <!-- Fontawesome -->
+    <link type="text/css" href="{{ asset('assets/backend/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}"
+        rel="stylesheet">
+
+    <!-- Notyf -->
+    <link type="text/css" href="{{ asset('assets/backend/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
+
+    <!-- Volt CSS -->
+    <link type="text/css" href="{{ asset('assets/backend/css/volt.css') }}" rel="stylesheet">
+
+    <!-- DataTable -->
+    <link rel="stylesheet" href="{{ asset('assets/backend/vendor/datatables/css/dataTables.bootstrap5.min.css') }}">
+
+    <style>
+        .ck-editor__editable {
+            min-height: 170px;
+        }
+
+    </style>
