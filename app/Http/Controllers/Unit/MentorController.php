1b2385100bb289872bc3b1b76bc98797f5dce86a
hafet17 <do.crazy192@gmail.com>
2021-10-01 11:07:27 +0700

    implement datatable mentor

--- app/Http/Controllers/Unit/MentorController.php
+++ app/Http/Controllers/Unit/MentorController.php
@@ -42,7 +42,7 @@ class MentorController extends Controller
                         </div>';
                 })
                 ->editColumn('gambar_mentor', function ($item) {
-                    return '<img src="' . Storage::url('public/'.$item->foto) . '" style="max-height: 40px;"/>';
+                    return '<img src="' . Storage::url('public/' . $item->foto) . '" style="max-height: 40px;"/>';
                 })
                 ->rawColumns(['option', 'gambar_mentor'])
                 ->make();
@@ -112,30 +112,67 @@ class MentorController extends Controller
 
     public function penempatan()
     {
-        return view('unit.mentor.penempatan_index', [
-            'mentor' => MentorKursus::with(['mentor','kursus_unit'])
-                                    ->whereHas('kursus_unit', function ($q) {
-                                        $q->where('unit_id', Auth::id());
-                                    })
-                                    ->get()
-        ]);
+        if (request()->ajax()) {
+            $query = MentorKursus::query()->with(['kursus_unit', 'mentor'])
+                ->whereHas('kursus_unit', function ($q) {
+                    $q->where('unit_id', Auth::id());
+                })
+                ->latest();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return
+                        '<div class="btn-group">
+                            <button class="btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                                <span class="icon icon-sm">
+                                    <span class="fas fa-ellipsis-h icon-dark"></span>
+                                </span>
+                                <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                <a class="dropdown-item" href="' . route('penempatan.edit', $item->id) . '"><span
+                                class="fas fa-edit mr-2"></span>Detail</a>  
+                                 <form action="' . route('penempatan.delete', $item->id) . '" method="POST">
+                                    ' . method_field('delete') . csrf_field() . '
+                                    <button id="deleteButton" type="submit" class="dropdown-item text-danger" data-name="' . $item->nama .  '">
+                                        <span class="fas fa-trash-alt mr-2"></span>Hapus</a>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>';
+                })
+                ->addColumn('nama_mentor', function ($item) {
+                    return $item->mentor->nama_mentor;
+                })
+                ->addColumn('foto', function ($item) {
+                    return '<img src="' . Storage::url('public/' . $item->mentor->foto) . '" style="max-height: 40px;"/>';
+                })
+                ->addColumn('kursus', function ($item) {
+                    return $item->kursus_unit->kursus->nama_kursus;
+                })
+                ->rawColumns(['foto', 'action'])
+                ->make();
+        }
+
+        return view('unit.mentor.penempatan_index');
     }
 
     public function create_penempatan()
     {
         $mentor = Mentor::where('unit_id', Auth::id())->get();
         $kursus_unit = KursusUnit::with('kursus')->where('unit_id',  Auth::id())->get();
-        return view('unit.mentor.penempatan_create', compact('mentor','kursus_unit'));
+        return view('unit.mentor.penempatan_create', compact('mentor', 'kursus_unit'));
     }
 
     public function store_penempatan(Request $request)
     {
         $request->validate([
-            'mentor_id' => 'required|numeric' ,
-            'kursus_unit_id' => 'required|numeric' ,
+            'mentor_id' => 'required|numeric',
+            'kursus_unit_id' => 'required|numeric',
             'pengalaman' => 'required'
         ]);
-        
+
         $cek_data = MentorKursus::where('mentor_id', $request->mentor_id)->where('kursus_unit_id', $request->kursus_unit_id)->first();
 
         if ($cek_data) {
@@ -146,11 +183,10 @@ class MentorController extends Controller
                 'kursus_unit_id' => $request->kursus_unit_id,
                 'pengalaman' => $request->pengalaman
             ]);
-    
+
             return redirect()->route('penempatan.index')
                 ->with(['status' => 'Data Mentor Berhasil Ditambahkan']);
         }
-        
     }
 
     public function edit_penempatan(MentorKursus $id)
@@ -167,15 +203,15 @@ class MentorController extends Controller
     public function update_penempatan(Request $request, $id)
     {
         $request->validate([
-            'mentor_id' => 'required|numeric' ,
-            'kursus_unit_id' => 'required|numeric' ,
+            'mentor_id' => 'required|numeric',
+            'kursus_unit_id' => 'required|numeric',
             'pengalaman' => 'required'
         ]);
-        
+
         $cek_data = MentorKursus::where('mentor_id', $request->mentor_id)
-                                ->where('kursus_unit_id', $request->kursus_unit_id)
-                                ->where('id', '!=', $id)
-                                ->first();
+            ->where('kursus_unit_id', $request->kursus_unit_id)
+            ->where('id', '!=', $id)
+            ->first();
 
         if ($cek_data) {
             return redirect()->back()->with(['warning' => 'Mentor dan kursus tersebut sudah ada sebelumnya.']);
@@ -183,7 +219,7 @@ class MentorController extends Controller
             $mentor_kursus = MentorKursus::find($id);
             $data = $request->all();
             $mentor_kursus->update($data);
-    
+
             return redirect()->route('penempatan.index')
                 ->with(['status' => 'Data Mentor Berhasil Diupdate.']);
         }
