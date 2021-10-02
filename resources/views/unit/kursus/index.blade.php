9baba72dd12360ef67a8e919195b6e373e9bf77f
hafet17 <do.crazy192@gmail.com>
2021-10-02 15:02:12 +0700

    layouting index kursus unit

--- resources/views/unit/kursus/index.blade.php
+++ resources/views/unit/kursus/index.blade.php
@@ -4,51 +4,51 @@
 
 @section('content')
 
-<div class="row mb-3">
-    <div class="col-sm-12">
-
-        <div class="card border-light shadow-sm components-section">
-            <div class="card-header">
-                Pilihan kursus
-            </div>
-            <div class="card-body card-block">
-                <form action="#" method="post" class="form-horizontal">
-                    <div class="row form-group">                            
-                        @foreach ($list_kursus as $kursus)
-                            <div class="col-6 pt-3 form-check form-switch">
-                                <input type="checkbox" class="form-check-input js-switch"  id="flexSwitchCheckChecked" data-id ="{{ $kursus->id }}" {{ $kursus->kursus_unit->contains('unit_id',Auth::user()->id) ? 'checked' : '' }}> {{ $kursus->nama_kursus }}
-                            </div>
-                        @endforeach
-                    </div>
-                </form>
-            </div>
-            <div class="card-footer"><span class="text-warning font-weight-bold">Pilihan kursus boleh lebih dari satu</span></div>
-        </div>
-    </div>
-
-   
-    </div>
-
-      <div class="row my-3">
-        @foreach ($kursus_unit as $item)
-        <div class="col-md-4 my-2">
-            <div class="card" style="width: 20rem;">
-                <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">{{ $item->kursus->nama_kursus }}</h5>
-                  <a href="{{ route('unit.kursus.add',$item->kursus_id) }}" class="btn btn-primary float-right"> <i class="fas fa-eye"></i> Detail</a>
+<div class="container-fluid">
+    <div class="row my-3">
+        <div class="col-sm-12">
+    
+            <div class="card border-light shadow-sm components-section">
+                <div class="card-header">
+                   <h5> Pilihan Kursus</h5>
                 </div>
-              </div>
+                <div class="card-body card-block">
+                    <form action="#" method="post" class="form-horizontal">
+                        <div class="row form-group">                            
+                            @foreach ($list_kursus as $kursus)
+                                <div class="col-6 pt-3 form-check form-switch">
+                                    <input type="checkbox" class="form-check-input js-switch"  id="flexSwitchCheckChecked" data-id ="{{ $kursus->id }}" {{ $kursus->kursus_unit->contains('unit_id',Auth::user()->id) ? 'checked' : '' }}> {{ $kursus->nama_kursus }}
+                                </div>
+                            @endforeach
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer"><span class="text-warning font-weight-bold">Pilihan kursus boleh lebih dari satu</span></div>
+            </div>
         </div>
-        @endforeach
-
-      </div>
-
-      <nav aria-label="Page navigation example">
-        <ul class="pagination pagination-template d-flex ">
-            {{ $kursus_unit->appends(Request::all())->links() }}
-        </ul>
-    </nav>
+        </div>
+    
+          <div class="row my-3">
+            @foreach ($kursus_unit as $item)
+            <div class="col-md-4 my-2">
+                <div class="card" style="width: 20rem;">
+                    <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ $item->kursus->nama_kursus }}</h5>
+                      <a href="{{ route('unit.kursus.add',$item->kursus_id) }}" class="btn btn-primary float-right"> <i class="fas fa-eye"></i> Detail</a>
+                    </div>
+                  </div>
+            </div>
+            @endforeach
+    
+          </div>
+    
+          <nav aria-label="Page navigation example">
+            <ul class="pagination pagination-template d-flex ">
+                {{ $kursus_unit->appends(Request::all())->links() }}
+            </ul>
+        </nav>
+</div>
 
 @endsection
 
