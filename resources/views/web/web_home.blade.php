35ffefdae6484dbcb7822763144b13786aa0f8cf
hafet17 <do.crazy192@gmail.com>
2021-10-02 11:41:12 +0700

    add button lihat kursus

--- resources/views/web/web_home.blade.php
+++ resources/views/web/web_home.blade.php
@@ -53,8 +53,14 @@
                         <div class="w-100">
                             <div class="row -mt-5 text-center mx-auto">
                                 <div class="col-xl-10 mx-auto">
-                                    <h1 class="display-4 font-weight-bold text-shadow kata1">{{$item->kata1}}</h1>
-                                    <p class="text-lg text-shadow kata2">{{ $item->kata2 }}</p>
+                                    <h1 class="font-weight-bold text-shadow kata1">Selamat Datang Di Bimble.id</h2>
+                                        <p class="text-lg text-shadow kata2 mt-3">{{ $item->kata2 }}</p>
+                                        <br>
+                                        <button class="btn btn-outline-light btn-lg py-2" style="background-color: unset !important;
+                                    color: unset !important; border-radius: 40px;">
+                                            <a href="#mulai" class="text-light" style="text-decoration: none">Lihat
+                                                Kursus </a>
+                                            <i class="fas fa-angle-right"></i> </button>
                                 </div>
                             </div>
                         </div>
@@ -103,10 +109,100 @@
         </div>
     </div>
 
-    <section class="py-6 ">
+
+
+    <section class="py-5" id="mulai">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-8">
+                    <h4>Rekomendasi Kursus</h4>
+
+                    @if(Request::get('keyword'))
+                    <h6 class="mt-2">Pencarian: <i> {{ Request::get('keyword')}} </i></h6>
+                    @endif
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="owl-carousel">
+
+                        @foreach ($kursus_kelompok as $item)
+                        <div data-marker-id="59c0c8e322f3375db4d89128" class="w-100 h-100 hover-animate">
+                            <div class="card card-kelas h-100 border-0 shadow">
+                                <div class="card-img-top overflow-hidden gradient-overlay">
+                                    <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}"
+                                        alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" /><a
+                                        href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
+                                        class="tile-link"></a>
+
+                                    <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
+                                        <div class="badge badge-transparent badge-pill px-3 py-2">
+                                            {{ $item->type->nama_type }}
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card-body d-flex align-items-center">
+                                    <div class="w-100">
+                                        <h6 class="card-title"><a
+                                                href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
+                                                class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a>
+                                        </h6>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+
+                        @foreach ($kursus_private as $item)
+                        <div data-marker-id="59c0c8e322f3375db4d89128" class="w-100 h-100 hover-animate">
+                            <div class="card card-kelas h-100 border-0 shadow">
+                                <div class="card-img-top overflow-hidden gradient-overlay">
+                                    <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}"
+                                        alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" /><a
+                                        href="{{ route('front.detail.private', $item->kursus->slug) }}"
+                                        class="tile-link"></a>
+
+                                    <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
+                                        <div class="badge badge-transparent badge-pill px-3 py-2">
+                                            {{ $item->type->nama_type }}
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="card-body d-flex align-items-center">
+                                    <div class="w-100">
+                                        <h6 class="card-title"><a
+                                                href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
+                                                class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a>
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+
+
+                    </div>
+                </div>
+
+                <div class="col-md-12 d-lg-flex align-items-center justify-content-end">
+                    <a href="{{ route('front.kursus') }}" class="text-primary text-sm"> Lihat Semua<i
+                            class="fas fa-angle-double-right ml-2"></i></a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="py-5 bg-gray-100">
         <div class="container">
             <div class="text-center pb-lg-4">
-                <h2 class="mb-5">Kenapa Memilih Bimble.id</h2>
+                <h2 class="my-5">Kenapa Memilih Bimble.id</h2>
             </div>
             <div class="row">
                 <div class="col-lg-4 mb-3 mb-lg-0 text-center">
@@ -144,154 +240,92 @@
     </section>
 
 
-    <section class="pt-5 pb-6" id="mulai">
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-md-8">
-                    <h4>Rekomendasi Kursus</h4>
-
-                    @if(Request::get('keyword'))
-                    <h6 class="mt-2">Pencarian: <i> {{ Request::get('keyword')}} </i></h6>
-                    @endif
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12">
-                    
-                    <div class="owl-carousel">
-
-                        @foreach ($kursus_kelompok as $item)
-                        <div data-marker-id="59c0c8e322f3375db4d89128" class="w-100 h-100 hover-animate">
-                            <div class="card card-kelas h-100 border-0 shadow">
-                                <div class="card-img-top overflow-hidden gradient-overlay">
-                                    <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}"
-                                        alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" /><a
-                                        href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
-                                        class="tile-link"></a>
-
-                                        <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
-                                            <div class="badge badge-transparent badge-pill px-3 py-2">
-                                                {{ $item->type->nama_type }}
-                                            </div>
-                                        </div>
-            
-                                </div>
-                                <div class="card-body d-flex align-items-center">
-                                    <div class="w-100">
-                                        <h6 class="card-title"><a
-                                                href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
-                                                class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a>
-                                        </h6>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-
-                        @foreach ($kursus_private as $item)
-                        <div data-marker-id="59c0c8e322f3375db4d89128" class="w-100 h-100 hover-animate">
-                            <div class="card card-kelas h-100 border-0 shadow">
-                                <div class="card-img-top overflow-hidden gradient-overlay">
-                                    <img src="{{ url('assets/images/kursus/'. $item->kursus->gambar_kursus) }}"
-                                        alt="{{ $item->kursus->nama_kursus }}" class="img-fluid" /><a
-                                        href="{{ route('front.detail.private', $item->kursus->slug) }}"
-                                        class="tile-link"></a>
-
-                                        <div class="card-img-overlay-top d-flex justify-content-between align-items-center">
-                                            <div class="badge badge-transparent badge-pill px-3 py-2">
-                                                {{ $item->type->nama_type }}
-                                            </div>
-                                        </div>
-            
-                                </div>
-                                <div class="card-body d-flex align-items-center">
-                                    <div class="w-100">
-                                        <h6 class="card-title"><a
-                                                href="{{ route('front.detail.kelompok', $item->kursus->slug) }}"
-                                                class="text-decoration-none text-dark">{{ $item->kursus->nama_kursus }}</a>
-                                        </h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-
-
-                    </div>
-                </div>
-
-                <div class="col-md-12 d-lg-flex align-items-center justify-content-end">
-                    <a href="{{ route('front.kursus') }}" class="text-primary text-sm"> Lihat Semua<i
-                            class="fas fa-angle-double-right ml-2"></i></a>
-                </div>
-            </div>
-
-        </div>
-    </section>
 
     <!-- Section Testimonial -->
-    <section class="py-3 mb-3">
+    <section class="py-5 mb-3">
         <div class="container">
-          <div class="text-center">
-           
-            <h2 class="mb-5">Testimoni Alumni</h2>
-          </div>
-          <!-- Slider main container-->
-          <div class="swiper-container testimonials-slider testimonials">
-            <!-- Additional required wrapper-->
-            <div class="swiper-wrapper pt-2 pb-5">
-              <!-- Slides-->
-              <div class="swiper-slide px-3">
-                <div class="testimonial card rounded-lg shadow border-0">
-                  <div class="testimonial-avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMx1itTXTXLB8p4ALTTL8mUPa9TFN_m9h5VQ&usqp=CAU" alt="..." class="img-fluid"></div>
-                  <div class="text">
-                    <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
-                    <p class="testimonial-text">
-                        Belajar di bimble.id adalah langkah yang tepat. materi sangat mudah dipahami, detail dan selalu ada ilmu baru yang dipelajari    
-                    </p><strong>Deddy Sujarwadi C.G</strong>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide px-3">
-                <div class="testimonial card rounded-lg shadow border-0">
-                  <div class="testimonial-avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmE--NjTTosid1jFeMFdc12EVQtKi7XPRMYqeHI0_4jJlqBanUiyQ-KrqN5tsdK_MO0j8&usqp=CAU" alt="..." class="img-fluid"></div>
-                  <div class="text">
-                    <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
-                    <p class="testimonial-text">Bimble.id adalah platform kursus offline yang tepat untuk belajar banyak hal. Materinya lengkap, semua dijelaskan dari dasar dan tentunya sangat mudah dipahami.</p><strong>M Hafid Masruri</strong>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide px-3">
-                <div class="testimonial card rounded-lg shadow border-0">
-                  <div class="testimonial-avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv3v8eT0Vp-0n3xYiISR6P-5Q1GZ0zoKHUQbXd-66yiWE2W4mmpO5LQfk0tUnpAvo54KU&usqp=CAU" alt="..." class="img-fluid"></div>
-                  <div class="text">
-                    <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
-                    <p class="testimonial-text">Bimble.id adalah rekomendasi platform kursus yang sangat cocok bagi pemula khususnya di bidang IT</p><strong>Fatih Nauval Azzidan</strong>
-                  </div>
-                </div>
-              </div>
-              <div class="swiper-slide px-3">
-                <div class="testimonial card rounded-lg shadow border-0">
-                  <div class="testimonial-avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyg0cHzGlwYy3wEgrjyuPdWSJZBm50KIxB4N4Y5h73lCfWXg60UddPtW6e1F_GEG1LMQo&usqp=CAU" alt="..." class="img-fluid"></div>
-                  <div class="text">
-                    <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
-                    <p class="testimonial-text">Bimble.id adalah salah satu platform kursus dengan kurikulum yang profesional serta dimentori oleh orang-orang yang expert dibindang nya</p><strong>Hasyim Asy'ari</strong>
-                  </div>
-                </div>
-              </div>
-             
+            <div class="text-center">
+
+                <h2 class="mb-5">Testimoni Alumni</h2>
             </div>
-            <div class="swiper-pagination">     </div>
-          </div>
+            <!-- Slider main container-->
+            <div class="swiper-container testimonials-slider testimonials">
+                <!-- Additional required wrapper-->
+                <div class="swiper-wrapper pt-2 pb-5">
+                    <!-- Slides-->
+                    <div class="swiper-slide px-3">
+                        <div class="testimonial card rounded-lg shadow border-0">
+                            <div class="testimonial-avatar"><img
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMx1itTXTXLB8p4ALTTL8mUPa9TFN_m9h5VQ&usqp=CAU"
+                                    alt="..." class="img-fluid"></div>
+                            <div class="text">
+                                <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                                <p class="testimonial-text">
+                                    Belajar di bimble.id adalah langkah yang tepat. materi sangat mudah dipahami, detail
+                                    dan selalu ada ilmu baru yang dipelajari
+                                </p><strong>Deddy Sujarwadi C.G</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide px-3">
+                        <div class="testimonial card rounded-lg shadow border-0">
+                            <div class="testimonial-avatar"><img
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmE--NjTTosid1jFeMFdc12EVQtKi7XPRMYqeHI0_4jJlqBanUiyQ-KrqN5tsdK_MO0j8&usqp=CAU"
+                                    alt="..." class="img-fluid"></div>
+                            <div class="text">
+                                <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                                <p class="testimonial-text">Bimble.id adalah platform kursus offline yang tepat untuk
+                                    belajar banyak hal. Materinya lengkap, semua dijelaskan dari dasar dan tentunya
+                                    sangat mudah dipahami.</p><strong>M Hafid Masruri</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide px-3">
+                        <div class="testimonial card rounded-lg shadow border-0">
+                            <div class="testimonial-avatar"><img
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv3v8eT0Vp-0n3xYiISR6P-5Q1GZ0zoKHUQbXd-66yiWE2W4mmpO5LQfk0tUnpAvo54KU&usqp=CAU"
+                                    alt="..." class="img-fluid"></div>
+                            <div class="text">
+                                <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                                <p class="testimonial-text">Bimble.id adalah rekomendasi platform kursus yang sangat
+                                    cocok bagi pemula khususnya di bidang IT</p><strong>Fatih Nauval Azzidan</strong>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide px-3">
+                        <div class="testimonial card rounded-lg shadow border-0">
+                            <div class="testimonial-avatar"><img
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyg0cHzGlwYy3wEgrjyuPdWSJZBm50KIxB4N4Y5h73lCfWXg60UddPtW6e1F_GEG1LMQo&usqp=CAU"
+                                    alt="..." class="img-fluid"></div>
+                            <div class="text">
+                                <div class="testimonial-quote"><i class="fas fa-quote-right"></i></div>
+                                <p class="testimonial-text">Bimble.id adalah salah satu platform kursus dengan kurikulum
+                                    yang profesional serta dimentori oleh orang-orang yang expert dibindang nya</p>
+                                <strong>Hasyim Asy'ari</strong>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="swiper-pagination"> </div>
+            </div>
         </div>
-      </section>
+    </section>
     <!-- End Section Testimonial -->
     @include('web.layouts.footer')
     @include('web.layouts.script')
     <script src="{{ asset('assets/frontend/vendor/Swiper/4.4.1/js/swiper.min.js') }}"></script>
+    <script src="{{ asset('assets/frontend/vendor/jquery/jquery.min.js') }}"></script>
+    <script>
+        $('a[href*=\\#]:not([href$=\\#])').click(function () {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 900);
+        });
+
+    </script>
 
 </body>
 
