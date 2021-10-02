6efca79ea466329c79499d0d1550e6297bcb2b58
hafet17 <do.crazy192@gmail.com>
2021-10-02 13:39:10 +0700

    re-layouting footer

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -4,43 +4,50 @@
      <div class="py-5 text-muted" style="background-color: #07112d">
          <div class="container">
              <div class="row mt-4">
-                 <div class="col-lg-4 mb-5 mb-lg-0">
-                     <h5 class="text-uppercase text-light mb-3">Bimble.id</h5>
-                     <p>
+                 <div class="col-lg-5 mb-5 mb-lg-0">
+                     <h4 class="text-capitalize text-light mb-3">Bimble.id</h4>
+                     <p style="margin-top: 42px;">
                          Bimble.id adalah situs penyedia kursus offline berbahasa Indonesia yang menyediakan berbagai
                          pembelajaran seperti komputer,design dsb
                      </p>
                  </div>
-                 <div class="col-lg-3 col-md-6 mb-lg-0">
-                     <h6 class="text-uppercase text-light mb-3">Company</h6>
-                     <ul class="list-unstyled mt-4">
+                 <div class=" col-lg-2 col-md-6 mb-lg-0">
+                     <h5 class="text-capitalize text-light mb-3">Company</h5>
+                     <ul class="list-unstyled" style="margin-top: 42px;">
                          <li class="mb-2"><a href="#" class="text-muted">Tentang</a></li>
                          <li class="mb-2"><a href="#" class="text-muted">Bantuan</a></li>
                          <li class="mb-2"><a href="#" class="text-muted">Kebijakan Privasi</a></li>
                      </ul>
                  </div>
-                 <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
-                     <h6 class="text-uppercase text-light mb-3">Customer</h6>
-                     <ul class="list-unstyled mt-4">
+                 <div class="col-lg-2 col-md-6 mb-5 mb-lg-0">
+                     <h5 class="text-capitalize text-light mb-3">Customer</h5>
+                     <ul class="list-unstyled" style="margin-top: 42px;">
                          <li class="mb-2"><a href="{{ route('siswa.login') }}" class="text-muted">Login</a></li>
                          <li class="mb-2"><a href="{{ route('siswa.register') }}" class="text-muted">Register</a></li>
                          <li class="mb-2"><a href="{{ route('front.kursus') }}" class="text-muted">Kursus</a></li>
                      </ul>
                  </div>
-                 <div class="col-lg-2">
-                     <h6 class="text-uppercase text-light mb-3">Follow US</h6>
-                     <ul class="list-inline mt-4">
+                 <div class="col-lg-3">
+                     <h5 class="text-capitalize text-light mb-3">Follow US</h5>
+                     <ul class="list-inline" style="margin-top: 42px;">
                          <li class="list-inline-item"><a href="#" target="_blank" title="facebook"
-                                 class="text-muted text-hover-primary"><i class="fab fa-2x fa-facebook"></i></a></li>
+                                 class="text-light text-hover-primary"><i class="fab fa-2x fa-facebook"></i></a></li>
                          <li class="list-inline-item"><a href="#" target="_blank" title="instagram"
-                                 class="text-muted text-hover-primary"><i class="fab fa-2x fa-instagram"></i></a></li>
+                                 class="text-light text-hover-primary"><i class="fab fa-2x fa-instagram"></i></a></li>
+                     </ul>
+                     <ul class="list-unstyled" style="margin-top: 35px">
+                         <li class="mb-2"><a href="#" style="color: #fff; font-size: 15px;">
+                                 Email: <span class="text-muted ml-2">bimble.id@gmail.com</span></a>
+                         </li>
+                         <li class="mb-2"><a href="#" style="color: #fff; font-size: 15px;">
+                                 Phone: <span class="text-muted ml-2"> +6285236639572</span></a></li>
                      </ul>
                  </div>
              </div>
          </div>
      </div>
      <!-- Copyright section of the footer-->
-     <div class="py-3 font-weight-light" style="background-color: #07112d;">
+     <div class=" py-3 font-weight-light" style="background-color: #07112d;">
          <div class="container">
              <div class="row align-items-center">
                  <div class="col-md-6 text-center text-md-left">
