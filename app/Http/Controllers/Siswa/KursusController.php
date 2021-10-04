49f6f214ba61e2b32eafa1b6c5f13b279c9b8ad5
hafet17 <do.crazy192@gmail.com>
2021-10-04 15:11:15 +0700

    refactor kursus controller

--- app/Http/Controllers/Siswa/KursusController.php
+++ app/Http/Controllers/Siswa/KursusController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Storage;
 
 class KursusController extends Controller
 {
-    public function pesan_kursus(Request $request, $kursus_unit_id)
+    public function pesan_kursus($kursus_unit_id)
     {
         if (KursusUnit::where('id', $kursus_unit_id)->exists()) {
 
@@ -22,8 +22,7 @@ class KursusController extends Controller
                 'status_sertifikat' => 'daftar'
             ]);
             return back()->withInput();
-
-        }else{
+        } else {
             return back()->withInput();
         }
     }
@@ -31,56 +30,53 @@ class KursusController extends Controller
     public function kursus()
     {
         $kursus_terima = SiswaKursus::with(['kursus_unit.kursus:id,nama_kursus', 'kursus_unit.unit:id,nama_unit'])
-                        ->where('siswa_id', Auth::id())
-                        ->where(function($q) {
-                            $q->where('status_sertifikat', 'terima')
-                              ->orWhere('status_sertifikat', 'lulus')
-                              ->orWhere('status_sertifikat', 'sertifikat');
-                          })
-                        ->get();
+            ->where('siswa_id', Auth::id())
+            ->where(function ($q) {
+                $q->where('status_sertifikat', 'terima')
+                    ->orWhere('status_sertifikat', 'lulus')
+                    ->orWhere('status_sertifikat', 'sertifikat');
+            })
+            ->get();
 
         $kursus_proses = SiswaKursus::with(['kursus_unit.kursus:id,nama_kursus', 'kursus_unit.unit:id,nama_unit'])
-                        ->where('siswa_id', Auth::id())
-                        ->where('status_sertifikat', 'daftar')
-                        ->get();
+            ->where('siswa_id', Auth::id())
+            ->where('status_sertifikat', 'daftar')
+            ->get();
 
-        // dd($kursus_terima);
-        return view('web.web_profile', compact('kursus_proses','kursus_terima'));
+        return view('web.web_profile', compact('kursus_proses', 'kursus_terima'));
     }
 
     public function materi($kursus_unit_id)
     {
         $cek_siswa = SiswaKursus::where('siswa_id', Auth::id())
-                        ->where('kursus_unit_id', $kursus_unit_id)
-                        ->where('status_sertifikat', 'daftar')
-                        ->first();
-                        
+            ->where('kursus_unit_id', $kursus_unit_id)
+            ->where('status_sertifikat', 'daftar')
+            ->first();
+
         if ($cek_siswa) {
             return back()->withInput();
         } else {
             $kursus_unit = KursusUnit::find($kursus_unit_id);
             $materi = Materi::with(['kursus:id,nama_kursus', 'unit:id,nama_unit'])
-                        ->where('kursus_id', $kursus_unit->kursus->id)
-                        ->where('unit_id', $kursus_unit->unit->id)
-                        ->get();
-    
-            $owner = KursusUnit::with(['kursus:id,nama_kursus', 'unit:id,nama_unit'])->where('id',$kursus_unit_id)->first();
+                ->where('kursus_id', $kursus_unit->kursus->id)
+                ->where('unit_id', $kursus_unit->unit->id)
+                ->get();
 
-            return view('web.web_profile', compact('materi','owner'));
+            $owner = KursusUnit::with(['kursus:id,nama_kursus', 'unit:id,nama_unit'])->where('id', $kursus_unit_id)->first();
+
+            return view('web.web_profile', compact('materi', 'owner'));
         }
-        
-
     }
 
     public function sertifikat_index()
     {
         $kursus_lulus = SiswaKursus::with(['kursus_unit.kursus:id,nama_kursus', 'kursus_unit.unit:id,nama_unit'])
-                        ->where('siswa_id', Auth::id())
-                        ->where(function($q) {
-                            $q->where('status_sertifikat', 'lulus')
-                              ->orWhere('status_sertifikat', 'sertifikat');
-                        })
-                        ->get();
+            ->where('siswa_id', Auth::id())
+            ->where(function ($q) {
+                $q->where('status_sertifikat', 'lulus')
+                    ->orWhere('status_sertifikat', 'sertifikat');
+            })
+            ->get();
 
         return view('web.web_profile', compact('kursus_lulus'));
     }
@@ -95,9 +91,9 @@ class KursusController extends Controller
 
         if ($cek_data) {
             $extention = $request->file('file')->extension();
-            $filename = Auth::id().'-'.date('dmyHis').'.'.$extention;
+            $filename = Auth::id() . '-' . date('dmyHis') . '.' . $extention;
             Storage::putFileAs('public/pembayaran', $request->file('file'), $filename);
-            
+
             $cek_data->update([
                 'file' => $filename
             ]);
@@ -105,5 +101,4 @@ class KursusController extends Controller
             return redirect()->back()->with(['status' => 'File Berhasil Diupload.']);
         }
     }
-
 }
