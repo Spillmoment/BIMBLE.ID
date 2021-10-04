874a4e27e687a0dffe3400164f027a7df04477c2
hafet17 <do.crazy192@gmail.com>
2021-10-04 15:10:51 +0700

    change name siswa

--- app/Http/Controllers/Admin/SiswaUnitController.php
+++ app/Http/Controllers/Admin/SiswaUnitController.php
@@ -16,7 +16,7 @@ class SiswaUnitController extends Controller
     public function index()
     {
         $unit = SiswaKursus::with(['siswa', 'kursus_unit'])
-                ->get()->groupBy('kursus_unit.unit_id')->toArray();
+            ->get()->groupBy('kursus_unit.unit_id')->toArray();
 
         foreach ($unit as $key => $element) {
             $data_id_unit[] = [$key];
@@ -33,14 +33,14 @@ class SiswaUnitController extends Controller
     public function detail_siswa($unit_id)
     {
         $siswa = SiswaKursus::with(['siswa', 'kursus_unit', 'kursus_unit.unit'])
-            ->whereHas('kursus_unit.unit', function($q) use ($unit_id) {
+            ->whereHas('kursus_unit.unit', function ($q) use ($unit_id) {
                 $q->where('unit_id', $unit_id);
             })
-            ->where(function($q) {
+            ->where(function ($q) {
                 $q->where('status_sertifikat', 'lulus')
                     ->orWhere('status_sertifikat', 'sertifikat');
             })
-            ->groupBy('siswa_id','kursus_unit_id')
+            ->groupBy('siswa_id', 'kursus_unit_id')
             ->paginate(9);
 
         return view('admin.siswa_unit.detail', [
@@ -50,10 +50,10 @@ class SiswaUnitController extends Controller
 
     public function confirm($id)
     {
-        $data = SiswaKursus::with(['siswa','kursus_unit'])->find($id);
+        $data = SiswaKursus::with(['siswa', 'kursus_unit'])->find($id);
         $pdf = PDF::loadView('admin.siswa_unit.pdf', compact('data'))->setPaper('F4', 'landscape');
-        $filename = $id.'-'.date('dmyHis').'.pdf';
-        Storage::put('public/sertifikat/'.$filename, $pdf->output());
+        $filename = $data->siswa->nama_siswa . '-' . date('dmyHis') . '.pdf';
+        Storage::put('public/sertifikat/' . $filename, $pdf->output());
 
         SiswaKursus::where('id', $id)
             ->update([
@@ -63,12 +63,12 @@ class SiswaUnitController extends Controller
 
         return back()->with(['status' => 'siswa berhasil dikonfirmasi']);
     }
-    
+
     public function confirm_down($id)
     {
-        $data = SiswaKursus::with(['siswa','kursus_unit'])->find($id);
+        $data = SiswaKursus::with(['siswa', 'kursus_unit'])->find($id);
 
-        Storage::delete('public/sertifikat/'.$data->file);
+        Storage::delete('public/sertifikat/' . $data->file);
 
         SiswaKursus::where('id', $id)
             ->update([
