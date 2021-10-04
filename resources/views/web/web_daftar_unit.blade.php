7ec66b6b7245f3cdc673fcf220a4cc1813b306d1
gunawan98 <deddygunawan98@gmail.com>
2021-10-04 19:43:30 +0700

    blade unit add select2

--- resources/views/web/web_daftar_unit.blade.php
+++ resources/views/web/web_daftar_unit.blade.php
@@ -1,6 +1,11 @@
 @extends('web.layouts.main')
 
 @section('title','Bimble | Daftar Unit')
+
+@push('style')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endpush
+
 @section('content')
 
 <div class="container-fluid py-5 px-lg-5">
@@ -29,9 +34,27 @@
                 <div class="mr-3" style="color: #322F56">
                      @if (Request::get('keyword') != null)
                      <span class="text-item text-capitalize"><strong> Unit {{ Request::get('keyword') }}</strong></span>
+                     @elseif (Request::get('place') != null)
+                     <span class="text-item text-capitalize"><strong> Unit dengan alamat {{ Request::get('place') }}</strong></span>
                      @else
                      <strong>Semua Unit</strong>
                      @endif
+                </div>
+
+                <div class="ml-auto">
+                    {{-- <form action="{{ route('front.kursus') }}" id="search" class="form-inline d-none d-sm-flex scroll" >
+                        <div
+                            class="input-label-absolute input-label-absolute-left input-reset input-expand ml-lg-2 ml-xl-3">
+                            <label for="search_search" class="label-absolute"><i class="fa fa-map-marker"></i><span class="sr-only">What are you looking for?</span></label>
+                            <input name="place" id='place_search' placeholder="Search" aria-label="Search"
+                                class="form-control form-control-sm border-0 shadow-lg-0 bg-gray-200" value="{{ Request::get('keyword') }}">
+                            <button type="reset" class="btn btn-reset btn-sm"><i class="fa-times fas"></i></button>
+                        </div>
+                    </form> --}}
+                    <form action="{{ route('unit.list') }}" method="get">
+                        <select class="cari-tempat form-control form-control-sm border-0 shadow-lg-0 bg-gray-200" style="width:300px;" name="place"></select>
+                        <button type="submit" class="btn btn-primary btn-sm"> <i class="fas fa-search mr-1"></i></button>
+                    </form>
                 </div>
 
             </div>
@@ -53,8 +76,8 @@
                         </div>
                         <div class="card-body d-flex align-items-center">
                             <div class="w-100">
-                                <h6 class="card-title"><a href="{{ route('unit.detail', $item->slug) }}"
-                                        class="text-decoration-none text-dark">{{ $item->nama_unit }}</a></h6>
+                                <h6 class="card-title"><a href="{{ route('unit.detail', $item->slug) }}" class="text-decoration-none text-dark">{{ $item->nama_unit }}</a></h6>
+                                <small class="text-black-50">{{ $item->alamat }}</small>
                             </div>
                         </div>
                     </div>
@@ -85,3 +108,43 @@
 </div>
 
 @endsection
+
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.cari-tempat').select2({
+      placeholder: 'Cari alamat...',
+      ajax: {
+        url: "{{ route('unit.getAutocomplte') }}",
+        dataType: 'json',
+        delay: 250,
+        processResults: function (data) {
+          return {
+            results:  $.map(data, function (item) {
+              return {
+                text: item.alamat,
+                id: item.alamat
+              }
+            })
+          };
+        },
+        cache: true
+      }
+    });
+
+    // $(".cari-tempat").on('change', function(e) {
+    //     $.ajax({
+    //         url: "{{ route('unit.list', ['keyword' => 'kori']) }}",
+    //         contentType: "application/json",
+    //         dataType: 'json',
+    //         success: function(result){
+    //             console.log('suc')
+    //         }
+    //     })
+    // });
+});
+
+  
+</script>
+@endpush
