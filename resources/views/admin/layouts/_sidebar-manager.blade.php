026473879cd2939f3394851a24ca1effdb671f17
hafet17 <do.crazy192@gmail.com>
2021-11-13 14:04:32 +0700

    refactor: sideabar admin

--- resources/views/admin/layouts/_sidebar-manager.blade.php
+++ resources/views/admin/layouts/_sidebar-manager.blade.php
@@ -34,27 +34,17 @@
                     <span>Dashboard</span>
                 </a>
             </li>
-            <li class="nav-item {{ (request()->is('manager/kategori*')) ? 'active' : '' }}">
-                <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-                    data-toggle="collapse" data-target="#submenu-kategori">
-                    <span>
-                        <span class="sidebar-icon"><span class="fas fa-tag"></span></span>
-                        Kategori
-                    </span>
-                    <span class="link-arrow"><span class="fas fa-chevron-right"></span></span>
-                </span>
-                <div class="multi-level collapse {{ (request()->is('manager/kategori*')) ? 'show' : '' }}" role="list"
-                    id="submenu-kategori" aria-expanded="false">
-                    <ul class="flex-column nav">
-                        <li class="nav-item {{ (Request::route()->getName() == 'kategori.create') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('kategori.create') }}"><span>Tambah Kategori</span></a>
-                        </li>
-                        <li class="nav-item {{ (Request::route()->getName() == 'kategori.index') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('kategori.index') }}"><span>Data Kategori</span></a>
-                        </li>
-                    </ul>
-                </div>
+
+            <!-- Sidebar Kategori -->
+            <li class="nav-item  {{ (request()->is('manager/kategori*')) ? 'active' : '' }}">
+                <a href="{{ route('kategori.index') }}" class="nav-link">
+                    <span class="sidebar-icon"><span class="fas fa-tag"></span></span>
+                    <span>Kategori</span>
+                </a>
             </li>
+
+
+            <!-- Sidebar Kursus -->
             <li
                 class="nav-item {{ (request()->is('manager/kursus*')) || (request()->is('manager/komentar*')) ? 'active' : '' }}">
                 <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
@@ -65,14 +55,14 @@
                     </span>
                     <span class="link-arrow"><span class="fas fa-chevron-right"></span></span>
                 </span>
-                <div class="multi-level collapse {{ (request()->is('manager/kursus*')) || (request()->is('manager/komentar*')) ? 'show' : '' }}"
+                <div class="multi-level collapse {{ (request()->is('manager/kursus*')) || (request()->is('manager/komentar*')) || (request()->is('manager/gallery')) ? 'show' : '' }}"
                     role="list" id="submenu-app" aria-expanded="false">
                     <ul class="flex-column nav">
-                        <li class="nav-item {{ (Request::route()->getName() == 'kursus.create') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('kursus.create') }}"><span>Tambah Kursus</span></a>
-                        </li>
                         <li class="nav-item {{ (Request::route()->getName() == 'kursus.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('kursus.index') }}"><span>Data Kursus</span></a>
+                        </li>
+                        <li class="nav-item {{ (Request::route()->getName() == 'gallery.index') ? 'active' : '' }}">
+                            <a class="nav-link" href="{{ route('gallery.index') }}"><span>Data Galeri</span></a>
                         </li>
                         <li class="nav-item {{ (Request::route()->getName() == 'komentar.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('komentar.index') }}"><span>Komentar Kursus</span></a>
@@ -80,27 +70,9 @@
                     </ul>
                 </div>
             </li>
-            <li class="nav-item {{ (request()->is('manager/gallery*')) ? 'active' : '' }}">
-                <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-                    data-toggle="collapse" data-target="#submenu-galerikursus">
-                    <span>
-                        <span class="sidebar-icon"><span class="fas fa-camera"></span></span>
-                        Galeri Kursus
-                    </span>
-                    <span class="link-arrow"><span class="fas fa-chevron-right"></span></span>
-                </span>
-                <div class="multi-level collapse {{ (request()->is('manager/gallery*')) ? 'show' : '' }}" role="list"
-                    id="submenu-galerikursus" aria-expanded="false">
-                    <ul class="flex-column nav">
-                        <li class="nav-item {{ (Request::route()->getName() == 'gallery.index') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('gallery.index') }}"><span>Data Galeri</span></a>
-                        </li>
-                        <li class="nav-item {{ (Request::route()->getName() == 'gallery.create') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('gallery.create') }}"><span>Tambah Galeri</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+
+
+            <!-- Sidebar Unit -->
             <li
                 class="nav-item {{ (request()->is('manager/unit*')) || request()->is('manager/pendaftar-unit*') || request()->is('manager/siswa-unit*') ? 'active' : '' }}">
                 <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
@@ -117,9 +89,6 @@
                         <li class="nav-item {{ (Request::route()->getName() == 'unit.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('unit.index') }}"><span>Data Unit</span></a>
                         </li>
-                        <li class="nav-item {{ (Request::route()->getName() == 'unit.create') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('unit.create') }}"><span>Tambah Unit</span></a>
-                        </li>
                         <li
                             class="nav-item {{ (Request::route()->getName() == 'pendaftar-unit.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('pendaftar-unit.index') }}"><span>Pendaftar
@@ -131,6 +100,8 @@
                     </ul>
                 </div>
             </li>
+
+            <!-- Sidebar  Pengaturan -->
             <li class="nav-item {{ (request()->is('manager/banner*')) ? 'active' : '' }}">
                 <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
                     data-toggle="collapse" data-target="#submenu-settings">
