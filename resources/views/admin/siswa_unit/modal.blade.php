0022f8e3b9cda7403cbfc6e4adb3dcda70e1f9d3
hafet17 <do.crazy192@gmail.com>
2021-11-14 11:21:13 +0700

    add: modal siswa unit

---
+++ resources/views/admin/siswa_unit/modal.blade.php
@@ -0,0 +1,31 @@
+
+  @isset($item->file)
+  <button class="btn btn-primary btn-sm" data-toggle="modal"
+      data-target="#modal-file"> <i class="fas fa-eye"></i> Detail
+  </button>
+  <!-- Modal Content -->
+  <div class="modal fade" id="modal-file" tabindex="-1" role="dialog"
+      aria-labelledby="modal-file" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h2 class="h6 modal-title">Detail Bukti Upload </h2>
+                  <button type="button" class="close" data-dismiss="modal"
+                      aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  <img src="{{ asset('storage/pembayaran/'.$item->file) }}"
+                      width="600" height="450">
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-link text-danger ml-auto"
+                      data-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
+  </div>
+  @else
+  N/A
+  @endisset
