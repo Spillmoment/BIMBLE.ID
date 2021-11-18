c741d05425025c2e38ca64e63947848b372212af
hafet17 <do.crazy192@gmail.com>
2021-11-18 11:25:12 +0700

    add: unit export excel

---
+++ app/Exports/UnitExports.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exports;
+
+use App\Unit;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class UnitExports implements FromView, ShouldAutoSize
+{
+    public function view(): View
+    {
+        return view('admin.unit.export', [
+            'unit' => Unit::latest()->get()
+        ]);
+    }
+}
