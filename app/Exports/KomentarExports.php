a1b00c23ccc90f124e1a70be10f6807d8b7b86c3
hafet17 <do.crazy192@gmail.com>
2021-11-20 01:10:07 +0700

    add: komentar exports

---
+++ app/Exports/KomentarExports.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exports;
+
+use App\Komentar;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+
+class KomentarExports implements FromView, ShouldAutoSize
+{
+    public function view(): View
+    {
+        return view('admin.komentar.export', [
+            'komentar' => Komentar::latest()->get()
+        ]);
+    }
+}
