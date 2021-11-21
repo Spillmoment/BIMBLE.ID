50fe3315570511ccab3cfd5d8315da6fdd093772
hafet17 <do.crazy192@gmail.com>
2021-11-21 13:53:52 +0700

    update: fix export unit

--- resources/views/admin/unit/export.blade.php
+++ resources/views/admin/unit/export.blade.php
@@ -3,9 +3,13 @@
         <tr>
             <th>No</th>
             <th>Nama Unit</th>
+            <th>Deskripsi</th>
+            <th>Alamat</th>
             <th>Email</th>
             <th>No Telepon</th>
-            <th>Alamat</th>
+            <th>Whats App</th>
+            <th>Telegram</th>
+            <th>Instagram</th>
             <th>Status</th>
         </tr>
     </thead>
@@ -14,10 +18,20 @@
         <tr>
             <td>{{ $loop->iteration }}</td>
             <td>{{ $item->nama_unit }}</td>
+            <td>
+                @php
+                $convert = html_entity_decode( $item->deskripsi );
+                $deskripsi = strip_tags($convert);
+                @endphp
+                {{ $deskripsi }}
+            </td>
+            <td>{{ $item->alamat }}</td>
             <td>{{ $item->email }}</td>
             <td>{{ $item->no_telp }}</td>
-            <td>{{ $item->alamat }}</td>
-            <td>{{ $item->status }}</td>
+            <td>{{ $item->whatsapp }}</td>
+            <td>{{ $item->telegram }}</td>
+            <td>{{ $item->instagram }}</td>
+            <td>{{ $item->status == '1' ? 'aktif' : 'nonaktif' }}</td>
         </tr>
         @endforeach
     </tbody>
