73fbdd7e01af0a8e1f75eee6c600d7df9a51bb2c
hafet17 <do.crazy192@gmail.com>
2021-11-23 23:22:45 +0700

    feat: implement filter type kursus in siswa unit

--- app/Http/Controllers/Admin/SiswaUnitController.php
+++ app/Http/Controllers/Admin/SiswaUnitController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Exports\Siswa\SiswaKelompok;
+use App\Exports\Siswa\SiswaPrivate;
 use App\Http\Controllers\Controller;
 use App\KursusUnit;
 use App\SiswaKursus;
 use App\Unit;
+use App\Type;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\DataTables;
 use PDF;
-
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class SiswaUnitController extends Controller
 {
@@ -30,20 +34,50 @@ class SiswaUnitController extends Controller
         ]);
     }
 
-    public function detail_siswa($unit_id)
+    public function detail_siswa(Request $request, $unit_id)
     {
-        $unit = KursusUnit::with('unit', 'kursus')->where('unit_id', $unit_id)->first();
+        $unit = KursusUnit::with('unit', 'kursus')
+            ->where('unit_id', $unit_id)->first();
+
+        $type = Type::latest()->get();
+        $private = Type::where('id', '1')->first();
+        $kelompok = Type::where('id', '2')->first();
+
 
         if (request()->ajax()) {
-            $query = SiswaKursus::with(['siswa', 'kursus_unit.kursus', 'kursus_unit.unit'])
-                ->whereHas('kursus_unit.unit', function ($q) use ($unit_id) {
-                    $q->where('unit_id', $unit_id);
-                })
-                ->where(function ($q) {
-                    $q->where('status_sertifikat', 'lulus')
-                        ->orWhere('status_sertifikat', 'sertifikat');
-                })
-                ->groupBy('siswa_id', 'kursus_unit_id');
+            $req = $request->type;
+            if (request()->input('type') != 0) {
+                $query = SiswaKursus::with(['siswa', 'kursus_unit.kursus', 'kursus_unit.unit', 'kursus_unit.type'])
+                    ->whereHas(
+                        'kursus_unit.unit',
+                        function ($q) use ($unit_id) {
+                            $q->where('unit_id', $unit_id);
+                        }
+                    )
+                    ->whereHas(
+                        'kursus_unit.type',
+                        function ($q) use ($req) {
+                            $q->where('type_id', $req);
+                        }
+                    )
+                    ->where(function ($q) {
+                        $q->where('status_sertifikat', 'lulus')
+                            ->orWhere('status_sertifikat', 'sertifikat');
+                    })
+                    ->groupBy('siswa_id', 'kursus_unit_id')
+                    ->latest();
+            } else {
+                $query = SiswaKursus::with(['siswa', 'kursus_unit.kursus', 'kursus_unit.unit', 'kursus_unit.type'])
+                    ->whereHas('kursus_unit.unit', function ($q) use ($unit_id) {
+                        $q->where('unit_id', $unit_id);
+                    })
+                    ->where(function ($q) {
+                        $q->where('status_sertifikat', 'lulus')
+                            ->orWhere('status_sertifikat', 'sertifikat');
+                    })
+                    ->groupBy('siswa_id', 'kursus_unit_id')
+                    ->latest();
+            }
 
             return DataTables::of($query)
                 ->addColumn('action', function ($item) {
@@ -75,6 +109,9 @@ class SiswaUnitController extends Controller
                 ->addColumn('kursus', function ($item) {
                     return $item->kursus_unit->kursus->nama_kursus ?? '';
                 })
+                ->addColumn('type', function ($item) {
+                    return ucwords($item->kursus_unit->type->nama_type ?? '');
+                })
                 ->editColumn('foto', function ($item) {
                     return '<img src="' . url('storage/siswa/' . $item->siswa->foto) . '" style="max-height: 40px;"/>';
                 })
@@ -94,26 +131,15 @@ class SiswaUnitController extends Controller
                             break;
                     }
                 })
-                ->editColumn('status_sertifikat', function ($item) {
-                    if ($item->status_sertifikat == 'sertifikat') {
-                        return '<a class="btn btn-warning btn-sm"
-                        href="' . route('siswa.unit.confirm_down', $item->id) . '"> <i
-                            class="fas fa-check"></i> Hapus Sertifikat </a>';
-                    } else {
-                        return '<a class="btn btn-primary btn-sm"
-                        href="' . route('siswa.unit.confirm', $item->id) . '"> <i
-                        class="fas fa-check"></i> Setujui </a>';
-                    }
-                })
-                ->editColumn('file', function ($item) {
-                    return view('admin.siswa_unit.modal', ['item' => $item]);
-                })
-                ->rawColumns(['action', 'foto', 'status', 'status_sertifikat', 'file'])
+                ->rawColumns(['action', 'foto', 'status'])
                 ->make();
         }
 
         return view('admin.siswa_unit.detail', [
-            'unit'  => $unit
+            'unit'  => $unit,
+            'type'  => $type,
+            'private' => $private,
+            'kelompok' => $kelompok
         ]);
     }
 
@@ -153,5 +179,25 @@ class SiswaUnitController extends Controller
     {
         $filepath = storage_path() . '/' . 'app' . '/public/sertifikat/' . $filename;
         return Response::download($filepath);
+    }
+
+    public function export_kelompok($id, $type)
+    {
+        $unit = KursusUnit::with('unit', 'kursus')
+            ->where('unit_id', $id)->first();
+        return Excel::download(
+            new SiswaKelompok($id, $type),
+            'Laporan-Siswa-Kelompok-Unit-' . $unit->unit->nama_unit .  '-' . now() . '.xlsx'
+        );
+    }
+
+    public function export_private($id, $type)
+    {
+        $unit = KursusUnit::with('unit', 'kursus')
+            ->where('unit_id', $id)->first();
+        return Excel::download(
+            new SiswaPrivate($id, $type),
+            'Laporan-Siswa-Private-Unit-' . $unit->unit->nama_unit .  '-' . now() . '.xlsx'
+        );
     }
 }
