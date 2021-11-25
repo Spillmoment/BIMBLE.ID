9fb2c6a27f1b45ec78a14d372a0cf6e2652b3a9e
hafet17 <do.crazy192@gmail.com>
2021-11-25 10:49:59 +0700

    salary controller

--- app/Http/Controllers/Unit/SalaryController.php
+++ app/Http/Controllers/Unit/SalaryController.php
@@ -11,32 +11,33 @@ use Yajra\DataTables\Facades\DataTables;
 class SalaryController extends Controller
 {
     public function index(Request $request)
-    {      
-        $status = Keuangan::with(['rule_gaji','unit'])
+    {
+        $status = Keuangan::with(['rule_gaji', 'unit'])
             ->where('status', 'active')
             ->orWhere('status', 'inactive')
             ->groupBy('status')
             ->get();
-        // dd($status);
+
         if (request()->ajax()) {
 
-            $query = Keuangan::with(['rule_gaji','unit'])
-                ->where('unit_id', Auth::id())
-                ->latest()
-                ->get();
-            
+            $query = Keuangan::query()
+                ->with(['rule_gaji', 'unit'])
+                ->where('status', 'active')
+                ->orWhere('status', 'inactive')
+                ->where('unit_id', Auth::id());
+
             return DataTables::of($query)
-                ->editColumn('created_at', function ($item){
+                ->editColumn('created_at', function ($item) {
                     return $item->created_at->format('d M Y');
                 })
                 ->addColumn('rule_gaji_lppk', function ($item) {
-                    return $item->rule_gaji->lppk.'%' ?? '';
+                    return $item->rule_gaji->lppk . '%' ?? '';
                 })
                 ->addColumn('rule_gaji_unit', function ($item) {
-                    return $item->rule_gaji->unit.'%' ?? '';
+                    return $item->rule_gaji->unit . '%' ?? '';
                 })
-                ->editColumn('nominal', function ($item){
-                    return 'Rp. '.number_format($item->nominal, 0, ',', '.');
+                ->editColumn('nominal', function ($item) {
+                    return 'Rp. ' . number_format($item->nominal, 0, ',', '.');
                 })
                 ->editColumn('status', function ($item) {
                     if ($item->status == 'active') {
@@ -48,7 +49,7 @@ class SalaryController extends Controller
                 ->rawColumns(['created_at', 'nominal', 'status'])
                 ->make(true);
         }
-        
+
         return view('unit.salary.index', compact('status'));
     }
 }
