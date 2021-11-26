ecf187512985dc6387a45fcaf5950d85d8bd7797
hafet17 <do.crazy192@gmail.com>
2021-11-26 23:44:27 +0700

    feat: Fix GalleryController

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -17,28 +17,7 @@ class GalleryController extends Controller
             $query = GaleriKursus::query()->with(['kursus'])->latest();
             return DataTables::of($query)
                 ->addColumn('action', function ($item) {
-                    return
-                        '<div class="btn-group">
-                            <button class="btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                <span class="icon icon-sm">
-                                    <span class="fas fa-ellipsis-h icon-dark"></span>
-                                </span>
-                                <span class="sr-only">Toggle Dropdown</span>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">        
-                                <a class="dropdown-item" href="' . route('gallery.show', $item->id) . '"><span
-                                        class="fas fa-eye mr-2"></span>Detail</a>
-                                <a class="dropdown-item" href="' . route('gallery.edit', $item->id) . '"><span
-                                        class="fas fa-edit mr-2"></span>Sunting</a>
-                                <form action="' . route('gallery.destroy', $item->id) . '" method="POST">
-                                    ' . method_field('delete') . csrf_field() . '
-                                    <button id="deleteButton" type="submit" class="dropdown-item text-danger" data-name="' . $item->kursus->nama_kursus .  '">
-                                        <span class="fas fa-trash-alt mr-2"></span>Hapus</a>
-                                    </button>
-                                </form>
-                            </div>
-                        </div>';
+                    return view('admin.gallery.action', compact('item'));
                 })
                 ->addColumn('kursus', function ($item) {
                     return $item->kursus->nama_kursus;
